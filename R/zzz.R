153e9d7af44a727fe35dab13923a8229bd78b8b4
Dylan Beaudette <dylanbeaudette@users.noreply.github.com>
2010-07-10 22:27:15 +0000

    updates. manual is now accessible as vignette

---
+++ R/zzz.R
@@ -0,0 +1,3 @@
+.First.lib <- function(lib, pkg)  {
+    packageStartupMessage("This is aqp ", utils::packageDescription("aqp", field="Version"), "\n", "see vignette('aqp-vignette') for the extended manual", appendLF = TRUE)
+}
