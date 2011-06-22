0804836909a002e05ea396cca2131e2ca235ed41
dylan <dylan@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-06-22 23:35:19 +0000

    working on mapunit geometry by bbox (SDA) functions

--- pkg/aqp/R/mapunit_geom_by_ll_bbox.R
+++ pkg/aqp/R/mapunit_geom_by_ll_bbox.R
@@ -1,7 +1,12 @@
 
-
+# 2011-06-22
+# It appears that SDA does not actually return the spatial intersecion of map unit polygons and bounding box. Rather, just those polygons that are completely within the bounding box.
 mapunit_geom_by_ll_bbox <- function(bbox)
   {
+  # temp fix until the new version of RGDAL is on CRAN
+  warning('sourcing SVN version of rgdal::readOGR()')
+  source('https://r-forge.r-project.org/scm/viewvc.php/*checkout*/pkg/R/ogr_sp.R?root=rgdal')
+  
   # check to make sure the user's OGR has been compiled with GML support:
   if( ! "GML" %in% ogrDrivers()$name)
 	stop('GML support is missing from your GDAL/OGR build.')
@@ -27,10 +32,9 @@ mapunit_geom_by_ll_bbox <- function(bbox)
   ## Error in data.frame(dlist, row.names = fids, stringsAsFactors = stringsAsFactors) : 
   ## duplicate row.names: 464331, 464429, 464445, [...]
   ## 
-  ## 
-  ## submitted a patch to Roger, waiting...
-  ## 
-  d <- readOGR(dsn=tf.full, layer='MapunitPoly')
+  ## submitted a patch to Roger, fix is in SVN
+  ## until then we need to source the current version of readOGR()
+  d <- readOGR(dsn=tf.full, layer='MapunitPoly', disambiguateFIDs=TRUE, stringsAsFactors=FALSE)
   
   
   # done, clean-up
