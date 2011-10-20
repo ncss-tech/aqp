b065621824468f3f3f0fcd834669d6d47b71a815
dylan <dylan@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-10-20 15:46:12 +0000

    stabilized SPC class, and fixed all related code and documentation. time for some more thinking/planning

--- pkg/aqp/R/Class-SoilProfileCollection.R
+++ pkg/aqp/R/Class-SoilProfileCollection.R
@@ -1,33 +1,31 @@
 setClass(
   Class='SoilProfileCollection', 
   representation=representation(
-    idcol="character", # column name containing IDs
-    topcol="character", # column name containing horizon top boundaries
-    bottomcol="character", # column name containing horizon bottom boundaries
-    metadata="data.frame", # single-row dataframe with key-value mapping
-    horizons="data.frame", # all horizons sorted by ID, top
-    site="data.frame", # data about the sampling sites
-    sp="SpatialPoints" # (optional) spatial data stored here
+    idcol='character', # column name containing IDs
+    depthcols='character', # 2 element vector with column names for hz top, bottom
+    metadata='data.frame', # single-row dataframe with key-value mapping
+    horizons='data.frame', # all horizons sorted by ID, top
+    site='data.frame', # data about the sampling sites
+    sp='SpatialPoints' # (optional) spatial data stored here
   ),
   prototype=prototype(
     idcol='id',
-    idcol='top',
-    idcol='bottom',
+    depthcols=c('top','bottom'),
     metadata=data.frame(stringsAsFactors=FALSE), # default units are unkown
     horizons=data.frame(),
     site=data.frame(),
-    sp=SpatialPoints(matrix(c(1,1), nrow=1))
+    sp=new('SpatialPoints')
   ),
   validity=function(object) {
     
 #     # number of ids and number of profiles must match
 #     if (length(which(!is.na(object@ids))) != length(object@profiles))
-#       stop("number of ids and number of profiles must match")
+#       stop('number of ids and number of profiles must match')
 #     # if there is some site data
 #     if (length(object@site) > 0) {
 #       # number of ids and number of sites must match  
 #       if (length(which(!is.na(object@ids))) != nrow(object@site))
-# 		 stop("number of ids and number of sites must match")
+# 		 stop('number of ids and number of sites must match')
 # 	}
 #     
     return(TRUE)
