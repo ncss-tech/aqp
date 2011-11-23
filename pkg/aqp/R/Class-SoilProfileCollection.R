996b95968826b9ddef98d93e844b2768f91e18ef
dylan <dylan@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-11-23 00:38:48 +0000

    tidying-up similar code blocks into their own files. adding some more sanity checks to SPC validity checking

--- pkg/aqp/R/Class-SoilProfileCollection.R
+++ pkg/aqp/R/Class-SoilProfileCollection.R
@@ -1,3 +1,52 @@
+test_hz_logic <- function(i, topcol, bottomcol)
+  {
+  # test for na
+  if(any(c(is.na(i[[topcol]])), is.na(i[[bottomcol]]))) {
+    res <- FALSE
+    names(res) <- 'hz_logic_pass'
+    return(res)
+  }
+  
+  ## this will not work when an SPC of discreet slices is generated!
+  ## todo: break out into smaller simpler functions, and use only when needed
+#   # test hz logic
+#   n <- nrow(i)
+#   res <- all.equal(i[[topcol]][-1], i[[bottomcol]][-n])
+#   if(res != TRUE) {
+#     res <- FALSE
+#     names(res) <- 'hz_logic_pass'
+#   }
+  
+  return(res)
+  }
+
+
+.SoilProfileCollectionValidity <- function(object) {
+    
+  
+  return(TRUE)
+  
+    # 1. test for bad horizon logic
+    id <- idname(object)
+    h <- horizons(object)
+    dc <- horizonDepths(object)
+    top <- dc[1]
+    bottom <- dc[2]
+    
+    # perform test
+    obj.test <- ddply(h, id, test_hz_logic, topcol=top, bottomcol=bottom)
+    
+    # let the user know which profile IDs aren't going to work
+    if(any(obj.test$hz_logic_pass == FALSE)) {
+      bad.ids <- obj.test[[id]][obj.test$hz_logic_pass == FALSE]
+      msg <- paste('\n\nNOTICE: invalid horizon logic in:', paste(bad.ids, collapse=','), '\n')
+      return(msg)
+    }
+      
+    # if all was well
+    return(TRUE)
+  }
+
 setClass(
   Class='SoilProfileCollection', 
   representation=representation(
@@ -16,20 +65,7 @@ setClass(
     site=data.frame(),
     sp=new('SpatialPoints')
   ),
-  validity=function(object) {
-
-#     # number of ids and number of profiles must match
-#     if (length(which(!is.na(object@ids))) != length(object@profiles))
-#       stop('number of ids and number of profiles must match')
-#     # if there is some site data
-#     if (length(object@site) > 0) {
-#       # number of ids and number of sites must match
-#       if (length(which(!is.na(object@ids))) != nrow(object@site))
-# 		 stop('number of ids and number of sites must match')
-# 	}
-#
-    return(TRUE)
-  }
+  validity=.SoilProfileCollectionValidity
 )
 
 
---
+++ pkg/aqp/R/SoilProfileCollection-spatial.R
@@ -0,0 +1,80 @@
+setMethod(f='proj4string', signature='SoilProfileCollection',
+  function(obj){
+    proj4string(obj@sp)
+  }
+)
+
+##
+## initialize spatial data
+##
+setReplaceMethod("coordinates", "SoilProfileCollection",
+  function(object, value) {
+
+  # basic sanity check... needs work
+  if(! inherits(value, "formula"))
+    stop('invalid formula')
+
+  # extract coordinates as matrix
+  mf <- data.matrix(model.frame(value, site(object), na.action=na.pass))
+
+  # test for missing coordinates
+  mf.missing <- apply(mf, 2, is.na)
+
+  if(any(mf.missing))
+	  stop('cannot initialize a SpatialPoints object with missing coordinates')
+
+  # assign to sp slot
+  # note that this will clobber any existing spatial data
+  object@sp <- SpatialPoints(coords=mf)
+  
+  # remove coordinates from source data
+  # note that mf is a matrix, so we need to access the colnames differently
+  coord_names <- dimnames(mf)[[2]]
+  idx <- match(coord_names, names(site(object)))
+  
+  # remove the named site data from site_data
+  # TODO we should use a proper setter!
+  object@site <- site(object)[, -idx]
+  
+  # done
+  return(object)
+  }
+)
+
+
+##
+## proj4string setting
+##
+setReplaceMethod("proj4string", "SoilProfileCollection",
+  function(obj, value) {
+  proj4string(obj@sp) <- value
+  obj
+  }
+)
+
+
+## spatial_subset: spatial clipping of a SPC (requires GEOS)
+if (!isGeneric("spatial_subset"))
+  setGeneric("spatial_subset", function(object, ...) standardGeneric("spatial_subset"))
+
+setMethod(f='spatial_subset', signature='SoilProfileCollection',
+  function(object, geom){
+
+    # This functionality require the GEOS bindings
+    # provided by rgeos
+    if(require(rgeos)) {
+      spc_intersection <- gIntersects(as(object, "SpatialPoints"), geom, byid = TRUE)
+      ids <- which(spc_intersection)
+
+      valid_ids <- site(object)[ids, object@idcol]
+
+      valid_horizons <- which(horizons(object)[, object@idcol] == valid_ids)
+      valid_sites <- which(site(object)[, object@idcol] == valid_ids)
+
+      SoilProfileCollection(idcol = object@idcol, depthcols = object@depthcols, horizons = horizons(object)[valid_horizons, ], site = site(object)[valid_sites, ], sp = object@sp[ids,])
+    }
+    else { # no rgeos, return original
+      stop('Spatial subsetting not performed, please install the `rgeos` package.')
+    }
+  }
+)
