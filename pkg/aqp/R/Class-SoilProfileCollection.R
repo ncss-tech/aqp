9bf21d36ab613c4a0252274f07bc81a411c8828c
dylan <dylan@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-12-12 23:01:42 +0000

    more bug-fixes, related to strictness of horizon logic testing, found a good compromise

--- pkg/aqp/R/Class-SoilProfileCollection.R
+++ pkg/aqp/R/Class-SoilProfileCollection.R
@@ -1,8 +1,19 @@
-test_hz_logic <- function(i, topcol, bottomcol)
+test_hz_logic <- function(i, topcol, bottomcol, strict=FALSE)
   {
   # test for na
   if(any(c(is.na(i[[topcol]])), is.na(i[[bottomcol]]))) {
     res <- FALSE
+    names(res) <- 'hz_logic_pass'
+    return(res)
+  }
+  
+  # test for illogical horizon boundaries
+  # note that this will fail with non-contiguous slices!
+  if(strict == TRUE) {
+    n <- nrow(i)
+    res <- all.equal(i[[topcol]][-1], i[[bottomcol]][-n])
+    if(res != TRUE)
+      res <- FALSE
     names(res) <- 'hz_logic_pass'
     return(res)
   }
