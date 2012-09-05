6c4b7119816757aee4c3e2ff52c3c1041a15eddc
Dylan Beaudette <dylanbeaudette@users.noreply.github.com>
2012-09-05 22:38:42 +0000

    documentation updates, bug fix in prepanel.depth_function

--- R/prepanel.depth_function.R
+++ R/prepanel.depth_function.R
@@ -1,29 +1,16 @@
-## check this function... 
-
-# TODO : this function doesn't help when the upper/lower CI limits aren't defined
-# revert to standard pre-panel function in that case
-# used to pre-compute wider xlim range based on upper/lower values
-prepanel.depth_function <- function(x, y, upper, lower, subscripts, groups=NULL, ...) {
-
-# working with grouped data and paneled data
-if(!missing(groups) & !missing(subscripts))
-	{
-	d <- data.frame(yhat=x, top=y, upper=upper[subscripts], lower=lower[subscripts], groups=groups[subscripts])
-	
-	# levels in the groups, for color matching
-	ll <- levels(d$groups)
-	}
-
-# no grouping, add a fake group for compatiblity
-if(missing(groups))
-	{
-	d <- data.frame(yhat=x, top=y, upper=upper[subscripts], lower=lower[subscripts], groups=factor(1))
-	# levels in the groups, for color matching
-	ll <- levels(d$groups)
-	}
+# computes proper limits when there is an 'upper' and 'lower' bound
+prepanel.depth_function <- function(x, y, upper, lower, subscripts, ...) {
+# composite into a data.frame
+d <- data.frame(yhat=x, top=y, upper=upper[subscripts], lower=lower[subscripts])
 
 # compute better xlim based on range of confidence band 
-the_range <- c(min(c(d$lower,d$yhat), na.rm=TRUE), max(c(d$upper, d$yhat), na.rm=TRUE))
-return(list(xlim=the_range))
+if (any(!is.na(x)) && any(!is.na(y))) {
+	the_range <- c(min(c(d$lower,d$yhat), na.rm=TRUE), max(c(d$upper, d$yhat), na.rm=TRUE))
+	return(list(xlim=the_range))
+}
+
+# if data are missing, well... we did the best we could
+else 
+	return(list(xlim=c(NA, NA)))
 }
 
