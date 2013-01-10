f6c728b24b36238ef776afc33148fc28877b0610
Dylan Beaudette <dylanbeaudette@users.noreply.github.com>
2013-01-10 21:36:11 +0000

    bug fixes and updates

--- R/zzz.R
+++ R/zzz.R
@@ -2,6 +2,6 @@
 #     packageStartupMessage("This is aqp ", utils::packageDescription("aqp", field="Version"), "\n", "see http://casoilresource.lawr.ucdavis.edu/drupal/taxonomy/term/56 for examples", appendLF = TRUE)
 # }
 
-.onLoad <- function(lib, pkg)  {
+.onAttach <- function(lib, pkg)  {
     packageStartupMessage("This is aqp ", utils::packageDescription("aqp", field="Version"), appendLF = TRUE)
 }
