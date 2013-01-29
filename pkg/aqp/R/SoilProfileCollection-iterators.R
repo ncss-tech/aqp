f1f083e54198e31121b0d9678e4a5ff988f4da45
dylan <dylan@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-01-29 19:45:55 +0000

    new functionality in profileApply, new examples

--- pkg/aqp/R/SoilProfileCollection-iterators.R
+++ pkg/aqp/R/SoilProfileCollection-iterators.R
@@ -1,10 +1,10 @@
 # analog to apply():
 # operates by profile, returns vector of length(object) or nrow(object)
 if (!isGeneric("profileApply"))
- 	setGeneric("profileApply", function(object, FUN, ...) standardGeneric("profileApply"))
+ 	setGeneric("profileApply", function(object, FUN, simplify=TRUE, ...) standardGeneric("profileApply"))
 
 setMethod(f='profileApply', signature='SoilProfileCollection',
-	function(object, FUN, ...) {
+	function(object, FUN, simplify=TRUE, ...) {
 						
 		# get profile IDs
 		pIDs <- profile_id(object)
@@ -18,8 +18,13 @@ setMethod(f='profileApply', signature='SoilProfileCollection',
 			l[[pID]] <- do.call(FUN, list(object[i, ], ...))
 		}
 		
-		# convert list into a vector
-		return(unlist(l))
+		# optionally simplify
+		if(simplify) {
+			res <- unlist(l)
+			return(res)
+		}
+		else
+			return(l)
 	}
 )
 
