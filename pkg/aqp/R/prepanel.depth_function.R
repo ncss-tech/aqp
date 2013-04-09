749ddc29e65029b2e5a7ccc2be8a1b553be76d76
dylan <dylan@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-04-09 20:35:03 +0000

    documentation, minor tweaks

--- pkg/aqp/R/prepanel.depth_function.R
+++ pkg/aqp/R/prepanel.depth_function.R
@@ -1,4 +1,5 @@
 # computes proper limits when there is an 'upper' and 'lower' bound
+# expands on either side by 5%
 prepanel.depth_function <- function(x, y, upper, lower, subscripts, ...) {
 # composite into a data.frame
 d <- data.frame(yhat=x, top=y, upper=upper[subscripts], lower=lower[subscripts])
@@ -6,6 +7,11 @@ d <- data.frame(yhat=x, top=y, upper=upper[subscripts], lower=lower[subscripts])
 # compute better xlim based on range of confidence band 
 if (any(!is.na(x)) && any(!is.na(y))) {
 	the_range <- c(min(c(d$lower,d$yhat), na.rm=TRUE), max(c(d$upper, d$yhat), na.rm=TRUE))
+	
+	# expand by 5%
+	the_range[1] <- the_range[1] - (the_range[1] * 0.05)
+	the_range[2] <- the_range[2] + (the_range[2] * 0.05)
+	
 	return(list(xlim=the_range))
 }
 
