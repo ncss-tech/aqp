9b800deeb4bbbf6751ed5e2ded3e4eb48c1a495e
Dylan Beaudette <dylanbeaudette@users.noreply.github.com>
2013-04-09 18:46:58 +0000

    panel.depth_function now syncs color of contributing fraction values with lines/areas

--- R/panel.depth_function.R
+++ R/panel.depth_function.R
@@ -2,7 +2,7 @@
 
 ## TODO: iterate over profile IDs instead of groups
 # note: confidence bands not defined when NA is present
-panel.depth_function <- function(x, y, id, upper=NA, lower=NA, subscripts=NULL, groups=NULL, sync.colors=FALSE, cf=NULL, ...) {
+panel.depth_function <- function(x, y, id, upper=NA, lower=NA, subscripts=NULL, groups=NULL, sync.colors=FALSE, cf=NA, ...) {
   
 # add grid
 panel.grid(h=-1, v=-1, lty=3, col=1)
@@ -33,7 +33,6 @@ if(length(y) > length(x)) {
 
 # normal plot -- not a step function
 else {
-	
 	# if we have an upper and lower bound defined, plot them
 	if(!missing(upper) & !missing(lower)) {
 		# working with grouped data and paneled data
@@ -70,15 +69,14 @@ else {
 			panel.polygon(x=c(d_i$lower, rev(d_i$upper)), y=c(d_i$top, rev(d_i$top)), col=region.col[m], border=NA, ...)
 			})
 		}
-	# no upper, lower bounds
+	
+	# no upper/lower polygon boundaries defined
 	else {
 		d <- data.frame(yhat=x, top=y, groups=groups[subscripts])
 		# levels in the groups, for color matching
 		ll <- levels(d$groups)	
 		n_groups <- length(ll)
 		}
-	
-	
 	
   # setup style parameters for main lines 
   # repeat enough times for the current number of groups
@@ -90,8 +88,7 @@ else {
 	by(d, d$groups, function(d_i){
 		# lookup color
 		m <- match(unique(d_i$group), ll)
-		
-		
+
 		# add line
 		panel.lines(d_i$yhat, d_i$top, lwd=line.lwd[m], col=line.col[m], lty=line.lty[m])
 		})
@@ -100,30 +97,46 @@ else {
 	
 	# TODO: might be interesting to plot group-wise CFs using same colors as lines / polygons
   # annotate with contributing fraction
-  if(! is.null(cf)) {
+  if(!missing(cf)) {
     
-  	# if plotting with grouped data, inform the user we are computing the mean CF / slice
-  	if(!missing(groups))
-  		warning('depth-wise mean contributing fraction values are printed', call.=FALSE)
-
-  	# aggregate group-wise to get a single CF / depth-slice
-  	cf.i.agg <- tapply(cf[subscripts], y, FUN=mean, na.rm=TRUE)
+#   	# if plotting with grouped data, inform the user we are computing the mean CF / slice
+#   	if(!missing(groups)) {
+#   		warning('depth-wise mean contributing fraction values are printed', call.=FALSE)
+#   		# add CF data to panel's worth of data
+#   		d$cf <- cf[subscripts]
+#   	}
+#   	else {
+#   		print(d)
+#   		print(cf)
+#   		d$cf <- cf[subscripts]
+#   	}
+		
+  	# add CF data to panel's worth of data
+  	d$cf <- cf[subscripts]
   	
-    # make a function for linear interpolation of CF values based on depth
-  	# note that we are using unique(y) as grouped data will repeat y n-group times
-    cf.approx.fun <- approxfun(unique(y), cf.i.agg, method='linear')
-    
-    # generate annotated depths: 5 cm to 95th percentile of max depth
-    y.q95 <- quantile(y, probs=c(0.95), na.rm=TRUE)
-    a.seq <- seq(from=5, to=y.q95, by=20)
-    
-    # interpolate CF at annotated depths
-    a.CF <- cf.approx.fun(a.seq)
-    a.text <- paste(round(a.CF * 100), '%')
-    
-    # add to right-hand side of the panel
-    unit <- gpar <- NULL
-    grid.text(a.text, x=unit(0.99, 'npc'), y=unit(a.seq, 'native'), just='right', gp=gpar(font=3, cex=0.8))  
+  	# annotate with contributing fraction by group
+  	by(d, d$groups, function(d_i){
+  		# lookup color
+  		m <- match(unique(d_i$group), ll)
+  		
+  		# make a function for linear interpolation of CF values based on depth
+  		cf.approx.fun <- approxfun(d_i$top, d_i$cf, method='linear')
+  		
+  		# generate annotated depths: 5 cm to 95th percentile of max depth
+  		y.q95 <- quantile(d_i$top, probs=c(0.95), na.rm=TRUE)
+  		a.seq <- seq(from=2, to=y.q95, by=20)
+  		
+  		a.seq <- a.seq + ((m-1) * 5)
+  		
+  		# interpolate CF at annotated depths
+  		a.CF <- cf.approx.fun(a.seq)
+  		a.text <- paste(round(a.CF * 100), '%')
+  		
+  		# add to right-hand side of the panel
+  		unit <- gpar <- NULL
+  		grid.text(a.text, x=unit(0.99, 'npc'), y=unit(a.seq, 'native'), just='right', gp=gpar(font=3, cex=0.8, col=line.col[m]))
+  	})
+      
   }
 
 }
