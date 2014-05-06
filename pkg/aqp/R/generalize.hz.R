6649ace4db208e30eaaa74083c24a4a57e0d3f4c
dylan <dylan@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-05-06 18:33:13 +0000

    bug fixes in generalize.hz and get.ml.hz

--- pkg/aqp/R/generalize.hz.R
+++ pkg/aqp/R/generalize.hz.R
@@ -1,9 +1,8 @@
 # return a vector of generalized horizon names, based on:
 # a vector of new names
 # a vector of patterns
-generalize.hz <- function(x, new, pat) {
-	# code for non-matching horizons
-	non.matching.code <- 'not-used'
+# code for any non-matching hz designations
+generalize.hz <- function(x, new, pat, non.matching.code='not-used') {
 	
 	# init vector of 'other', same length as original horizon name vector
 	g <- rep(non.matching.code, times=length(x))
