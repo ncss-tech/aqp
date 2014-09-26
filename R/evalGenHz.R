281181a4667aff1115c44e2a89bcb552765fb045
Dylan Beaudette <dylanbeaudette@users.noreply.github.com>
2014-09-26 18:31:10 +0000

    working on genhz docs and supporting new function in aqp, still needs Rd style doc

--- R/evalGenHz.R
+++ R/evalGenHz.R
@@ -1,24 +1,25 @@
-evalGenHZ <- function(obj, genhz, vars, non.matching.code='not-used', stand=TRUE, trace=FALSE) {
+## TODO: encode some kind of ID into the dissimilarity matrix for later use
+
+evalGenHZ <- function(obj, genhz, vars, non.matching.code='not-used', stand=TRUE, trace=FALSE, metric='euclidean') {
   # extract horizons
-  h <- horizons(obj)
+  h <- as(obj, 'data.frame')
   
   # make an index to complete data
   no.na.idx <- which(complete.cases(h[, vars]))
-    
-  # compute pair-wise dissimilarities using our variables of interest
-  d <- daisy(h[no.na.idx, vars], stand=stand)
   
-  # apply fudge-factor in case of duplicate data (0s in the dissimilarity matrix)
+  # compute pair-wise dissimilarities using our variables of interest
+  d <- daisy(h[no.na.idx, vars], stand=stand, metric=metric)
+  
+  # fudge-factor in case of duplicate data (0s in the dissimilarity matrix)
   fudge <- min(d) / 100
-  d <- d + fudge
   
   # perform non-metric MDS of dissimilarity matrix
-  mds <- isoMDS(d, trace=trace)
+  mds <- isoMDS(d + fudge, trace=trace)
   
   # compute silhouette widths after removing not-used genhz class
-  idx <-  which(complete.cases(h[, vars]) & h[[genhz]] != non.matching.code)
-  d <- daisy(h[idx, vars], stand=stand)
-  sil <- silhouette(as.numeric(h[[genhz]])[idx], d)
+  sil.idx <-  which(complete.cases(h[, vars]) & h[[genhz]] != non.matching.code)
+  d.sil <- daisy(h[sil.idx, vars], stand=stand)
+  sil <- silhouette(as.numeric(h[[genhz]])[sil.idx], d.sil)
     
   # add new columns
   h$mds.1 <- NA
@@ -29,18 +30,20 @@ evalGenHZ <- function(obj, genhz, vars, non.matching.code='not-used', stand=TRUE
   # copy values
   h$mds.1[no.na.idx] <- mds$points[, 1]
   h$mds.2[no.na.idx] <- mds$points[, 2]
-  h$sil.width[idx] <- sil[, 3]
-  h$neighbor[idx] <- levels(h[[genhz]])[sil[, 2]]
+  h$sil.width[sil.idx] <- sil[, 3]
+  h$neighbor[sil.idx] <- levels(h[[genhz]])[sil[, 2]]
   
-  # compute group-wise summaries
+  # melt into long form
   m <- melt(h, id.vars=genhz, measure.vars=c(vars, 'sil.width'))
+  
+  # compute group-wise summaries-- note that text is returned
   m.summary <- ddply(m, c(genhz, 'variable'), summarize, 
-                     stats=paste0(round(mean(value, na.rm=TRUE), 2), ' (' , sd=round(sd(value, na.rm=TRUE), 2), ')')
+                     stats=format(paste0(round(mean(value, na.rm=TRUE), 2), ' (' , sd=round(sd(value, na.rm=TRUE), 2), ')'), justify='right')
                      )
   fm <- paste0(genhz, ' ~ variable')
   genhz.stats <- cast(m.summary, fm, value='stats')
   
   # composite into a list
-  res <- list(horizons=h[, c('mds.1', 'mds.2', 'sil.width', 'neighbor')], stats=genhz.stats)
+  res <- list(horizons=h[, c('mds.1', 'mds.2', 'sil.width', 'neighbor')], stats=genhz.stats, dist=d)
   return(res)
 }
