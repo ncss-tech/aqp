41bf0c6ea765e510a48e70c6d32e4953a8e9902e
Dylan Beaudette <dylan.beaudette@gmail.com>
2017-01-03 14:18:39 -0800

    fixing URL, removing find_horizons... code for now, need some more low-level accessor functions first

--- R/SPC-find_horizons.R
+++ R/SPC-find_horizons.R
@@ -1,21 +1,31 @@
+
+### this isn't ready for prime time, better to use slice() for now
+
+### 
+
 ## TODO: this should be 1 of a series of functions built in to aqp
 ## find_horizons_by_points: locate horizons at distinct depths
 ## find_horizons_by_interval: locate horizons that overlap with a defined interval
-# spc: SPC of single profile
-# z: vector of depths to search for
-find_horizons_by_points <- function(spc, z) {
-  # sanity check
-  if(length(s) > 1)
-    stop('this function can only operate on a SoilProfileCollection containing 1 profile', call. = FALSE)
-  # get top/bottom names
-  hdc <- horizonDepths(spc)
-  h <- horizons(spc)[, hdc]
-  
-  # implicit iteration over elements of 'z'
-  idx <- sapply(z, function(i) {
-    which(i >= h[, 1] & i <= h[, 2])
-  })
-  
-  # index to horizons containing depths 'z'
-  return(idx)
-}
+
+# 
+# 
+# # spc: SoilProfileCollection
+# # z: vector of depths to search for
+# find_horizons_by_points <- function(spc, z) {
+#   # get top/bottom names
+#   hdc <- horizonDepths(spc)
+#   h <- horizons(spc)[, hdc]
+#   
+#   # implicit iteration over elements of 'z'
+#   idx <- sapply(z, function(i) {
+#     res <- i >= h[, 1] & i <= h[, 2]
+#     res[!res] <- NA
+#     return(res)
+#   })
+#   
+#   # unique index to horizons containing depths 'z'
+#   # this index is relative to the current soil profile in the collection
+#   return(unique(as.vector(idx)))
+# }
+# 
+# 
