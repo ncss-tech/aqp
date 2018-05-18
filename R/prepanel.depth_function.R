e9275b46b4292879fe0f2b162edea64aeb883bf4
Dylan Beaudette <dylan.beaudette@gmail.com>
2018-05-18 15:55:01 -0700

    fixinf #53

--- R/prepanel.depth_function.R
+++ R/prepanel.depth_function.R
@@ -1,22 +1,32 @@
 # computes proper limits when there is an 'upper' and 'lower' bound
-# expands on either side by 5%
-prepanel.depth_function <- function(x, y, upper, lower, subscripts, ...) {
-# composite into a data.frame
-d <- data.frame(yhat=x, top=y, upper=upper[subscripts], lower=lower[subscripts])
+# expands x-axis on either side of upper/lower (if present) by 5%
+prepanel.depth_function <- function(x, y, upper=NA, lower=NA, subscripts, ...) {
 
-# compute better xlim based on range of confidence band 
-if (any(!is.na(x)) && any(!is.na(y))) {
-	the_range <- c(min(c(d$lower,d$yhat), na.rm=TRUE), max(c(d$upper, d$yhat), na.rm=TRUE))
-	
-	# expand by 5%
-	the_range[1] <- the_range[1] - (the_range[1] * 0.05)
-	the_range[2] <- the_range[2] + (the_range[2] * 0.05)
-	
-	return(list(xlim=the_range))
-}
-
-# if data are missing, well... we did the best we could
-else 
-	return(list(xlim=c(NA, NA)))
+  # composite into a data.frame
+  if(!missing(upper) & !missing(lower)) {
+    d <- data.frame(yhat=x, top=y, upper=upper[subscripts], lower=lower[subscripts])
+  } else {
+    # no upper / lower - use central value as place-holder
+    d <- data.frame(yhat=x, top=y, upper=x, lower=x)
+  }
+  
+  
+  # compute better xlim based on range of upper/lower values
+  # requires some data in both x and y
+  if (any(!is.na(x)) && any(!is.na(y))) {
+    the_range <- c(min(c(d$lower,d$yhat), na.rm=TRUE), max(c(d$upper, d$yhat), na.rm=TRUE))
+    
+    # expand by 5%
+    the_range[1] <- the_range[1] - (the_range[1] * 0.05)
+    the_range[2] <- the_range[2] + (the_range[2] * 0.05)
+    
+    return(list(xlim=the_range))
+    
+  } else {
+    print('this is')
+    # if data are missing, well... we did the best we could
+    return(list(xlim=c(NA, NA)))
+  }
+  
 }
 
