84ec2f61ab293024eafecb862c04e0d1b965f96f
Dylan Beaudette <debeaudette@ucdavis.edu>
2018-12-21 23:30:31 -0800

    getting ready for next CRAN release

--- R/sketch.R
+++ R/sketch.R
@@ -4,18 +4,18 @@
 # https://github.com/ncss-tech/aqp/issues/62
 # 
 
-# x: SPC
-sketch <- function(x, ...) {
-  
-  
-}
-
-# this will replace PHP-GD code in SoilWeb
-# x: SPC, single profile (SoilProfile object in aqp 2.x)
-singleProfileSketch <- function(x, moistColor=NULL, dryColor=NULL, ...) {
-  
-  
-}
+# # x: SPC
+# sketch <- function(x, ...) {
+#   
+#   
+# }
+# 
+# # this will replace PHP-GD code in SoilWeb
+# # x: SPC, single profile (SoilProfile object in aqp 2.x)
+# singleProfileSketch <- function(x, moistColor=NULL, dryColor=NULL, ...) {
+#   
+#   
+# }
 
 
 
