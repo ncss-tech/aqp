5eb5e23645872221d962edbf32875f50ecf0547f
Dylan Beaudette <debeaudette@ucdavis.edu>
2018-12-27 13:36:03 -0800

    re-making sp5 with new SPC structure

--- misc/test_suite/process_OSACA-data.R
+++ misc/test_suite/process_OSACA-data.R
@@ -1,6 +1,7 @@
 library(aqp)
+library(plyr)
 
-x <- read.csv('OSACA-example_soil_data.csv')
+x <- read.csv('OSACA-example_soil_data.csv', stringsAsFactors = FALSE)
 
 # assume horizons are measured from 0
 x.new <- ddply(x, .(soil), .fun=function(i) {data.frame(i, top=c(0, i$depth[-length(i$depth)]), bottom=i$depth)})
@@ -9,21 +10,30 @@ x.new <- ddply(x, .(soil), .fun=function(i) {data.frame(i, top=c(0, i$depth[-len
 x.new$top <- as.integer(x.new$top)
 x.new$bottom <- as.integer(x.new$bottom)
 
-# figure out column names / meanings\
-# R,G,B appear to be color data
-x.new$soil_color <- rgb(x.new$R25, g=x.new$G25, b=x.new$B25)
-plot(depth ~ soil, data=x.new, col=x.new$soil_color, pch=15, ylim=c(400, 0))
-
-# SPC
+# init SPC
 depths(x.new) <- soil ~ top + bottom
 
-# give it a try: seems reasonable
-d <- profile_compare(x.new, vars=c('sand','Mg','EC'), replace_na=TRUE, max_d=100, k=0, add_soil_flag=TRUE)
+## max RGB value?
+# R,G,B appear to be color data
+x.new$soil_color <- rgb(x.new$R25, g=x.new$G25, b=x.new$B25, maxColorValue = 1)
 
-# save to .Rdata
+# plot(x.new[1:25, ])
+# plot(sample(x.new, 25))
+
+# metadata
+m <- metadata(x.new)
+m$citation <- "F. Carre, M.C. Girard. 2002. Quantitative mapping of soil types based on regression kriging of taxonomic distances with landform and land cover attributes. Geoderma. 110: 241-263."
+m$dateAdded <- Sys.Date()
+
+metadata(x.new) <- m
+
+# copy for sample data sp5
 sp5 <- x.new
+
+# cleanup
 sp5@horizons$depth <- NULL
 sp5@horizons$hor <- NULL
 sp5@horizons$soil <- as.character(sp5@horizons$soil)
 
-save(sp5, file='sp5.rda')
+# save to pacakge
+save(sp5, file='../../data/sp5.rda')
