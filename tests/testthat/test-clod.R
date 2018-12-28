30423daa363754aa96e902dfa6e9384073607281
Andrew Gene Brown <andrew.g.brown@ca.usda.gov>
2018-12-28 13:11:00 -0800

    crit.clay.argillic.Rd and fixing bad function name in test-clod.R

--- tests/testthat/test-clod.R
+++ tests/testthat/test-clod.R
@@ -11,9 +11,6 @@ test_that("aggregation of horizons by depth", {
     # intersection at a single depth should return only one horizon
     expect_equal(sum(clod(p, 50) %in% hzID(p)), 1)
 
-    #hz.dz is 5-keystroke alias of intersect.horizon()
-    expect_equal(clod(p, 25, 50), intersect.horizon(p, 25, 50))
-  
     # spc.by.z clods() your input SPC's horizons p by depths specified
     foo <- spc.by.z(p, 25, 100)
     
