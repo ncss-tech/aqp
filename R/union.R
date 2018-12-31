ba4e0d9b02c3bbeecabf5b657b9dc0f491920c3b
Dylan Beaudette <debeaudette@ucdavis.edu>
2018-12-30 23:34:28 -0800

    union is now able to handle non-conformal SPCs, #71

--- R/union.R
+++ R/union.R
@@ -24,95 +24,139 @@ rbind.SoilProfileCollection <- function(...) {
 
 
 # TODO: https://github.com/ncss-tech/aqp/issues/71
-#
-# TODO: concatenation of data with duplicated IDs in @site, but unique data in other @site fields, will result in corrupt SPC
-# TODO: duplicates in @sp will cause errors
-# TODO: duplicates are removed in all other slots... does this make sense?
-# NOTE: union is defined in base, do we need a better name?
-union <- function(spc=list(), method='all') {
+union <- function(spc=list(), method='all', drop.spatial=FALSE) {
   # setup some defaults
   options(stringsAsFactors=FALSE)
   
-  # parse dots
-  objects <- spc
-  names(objects) <- NULL
-  
-  # short-circuits
-  if(length(objects) == 0)
+    # short-circuits
+  if(length(spc) == 0)
     return(NULL)
-  if(length(objects) == 1)
-    return(objects[1])
+  if(length(spc) == 1)
+    return(spc[1])
   
-  ## TODO: normalize profile ID, horizon ID, horizon depths
-  # profile_id() <- 
-  # horizonDepths() <- 
-  # idname() <-
-  
-  
-  # combine pieces
-  # should have length of 1
-  o.idname <- unique(lapply(objects, idname))
-  o.depth.units <- unique(lapply(objects, depth_units))
-  o.hz.depths <- unique(lapply(objects, horizonDepths))
-  o.m <- unique(lapply(objects, aqp::metadata))
-  o.coords <- unique(lapply(objects, function(i) ncol(coordinates(i))))
-  o.p4s <- unique(lapply(objects, proj4string))
-  
-  # should have length > 1
-  o.h <- lapply(objects, horizons)
-  o.s <- lapply(objects, site)
-  o.d <- lapply(objects, diagnostic_hz)
-  o.sp <- lapply(objects, slot, 'sp')
-  
-  # sanity checks:
-  if(length(o.idname) > 1)
-    stop('inconsistent ID names', call.=FALSE)
+  # check for non-conformal depth units
+  o.depth.units <- unique(lapply(spc, depth_units))
   if(length(o.depth.units) > 1)
     stop('inconsistent depth units', call.=FALSE)
-  if(length(o.hz.depths) > 1)
-    stop('inconsistent depth columns', call.=FALSE)
-  if(length(o.m) > 1)
-    stop('inconsistent metadata', call.=FALSE)
   
-  # spatial data may be missing...
-  if(length(o.coords) > 1)
-    stop('inconsistent spatial data', call.=FALSE)
-  if(length(o.p4s) > 1)
-    stop('inconsistent CRS', call.=FALSE)
+  
+  # test for non-conformal CRS if keeping spatial data
+  if(!drop.spatial) {
+    # check for non-conformal CRS in @sp
+    o.p4s <- unique(lapply(spc, proj4string))
+    if(length(o.p4s) > 1)
+      stop('inconsistent CRS', call.=FALSE)
+  }
+  
+  
+  # decompose to list of lists
+  spc.list <- lapply(spc, as, 'list')
+  
+  # number of SPCs
+  n.spc <- length(spc.list)
+  # total profiles
+  n.profiles <- sum(sapply(spc, length))
+  
+  # template for combined data is based on the first element
+  new.pID <- spc.list[[1]]$idcol
+  new.hzd <- spc.list[[1]]$depthcols
+  new.metadata <- spc.list[[1]]$metadata
+  
+  # TODO: need a template for coordinate names if spatial data are present in all
+  
+  # reset profile ID names in all other objects
+  # also rest depth names
+  for(i in 2:n.spc) {
+    # save originals
+    old.pID <- spc.list[[i]]$idcol
+    old.hzd <- spc.list[[i]]$depthcols
+    
+    # profile ID in horizons
+    idx <- match(old.pID, names(spc.list[[i]]$horizons))
+    names(spc.list[[i]]$horizons)[idx] <- new.pID
+    
+    # profile ID in site
+    idx <- match(old.pID, names(spc.list[[i]]$site))
+    names(spc.list[[i]]$site)[idx] <- new.pID
+    
+    # profile ID in diagnostic, may be missing
+    if(old.pID %in% names(spc.list[[i]]$diagnostic)) {
+      idx <- match(old.pID, names(spc.list[[i]]$diagnostic))
+      names(spc.list[[i]]$diagnostic)[idx] <- new.pID
+    }
+    
+    # hz depth columns
+    idx <- match(old.hzd, names(spc.list[[i]]$horizons))
+    names(spc.list[[i]]$horizons)[idx] <- new.hzd
+    
+    # reset id names
+    spc.list[[i]]$idcol <- new.pID
+    spc.list[[i]]$depthcols <- new.hzd
+  }
+  
+  # extract pieces
+  o.h <- lapply(spc.list, '[[', 'horizons')
+  o.s <- lapply(spc.list, '[[', 'site')
+  o.d <- lapply(spc.list, '[[', 'diagnostic')
+  o.sp <- lapply(spc.list, '[[', 'sp')
   
   # generate new SPC components
   # using plyr::rbind.fill seems to solve the problem on non-conformal DF
   # https://github.com/ncss-tech/aqp/issues/71
-  o.h <- unique(do.call('rbind.fill', o.h)) # horizon data
-  o.s <- unique(do.call('rbind.fill', o.s)) # site data
-  o.d <- unique(do.call('rbind.fill', o.d)) # diagnostic data, leave as-is
+  o.h <- do.call('rbind.fill', o.h) # horizon data
+  o.s <- do.call('rbind.fill', o.s) # site data
+  o.d <- do.call('rbind.fill', o.d) # diagnostic data, leave as-is
   
-  # spatial points require some more effort when spatial data are missing
-  o.1.sp <- objects[[1]]@sp
-  
-  # missing spatial data
-  if(ncol(coordinates(o.1.sp)) == 1) {
-    o.sp <- o.1.sp # copy the first filler
+  if(! drop.spatial) {
+    # check for non-conformal coordinates
+    dim.coords <- sapply(o.sp, function(i) {
+      ncol(sp::coordinates(i))
+    })
+    
+    # note: an SPC with default @sp is a 1 column matrix
+    if(length(unique(dim.coords)) > 1) {
+      stop('non-conformal point geometry', call. = FALSE)
+    }
+    
+    ## TODO: this may not matter to rbind.SpatialPoints
+    # # check for variations in coordinate names
+    # coords.names <- lapply(o.sp, function(i) {
+    #   dimnames(coordinates(i))[[2]]
+    # })
+    # 
+    # if(length(unique(coords.names)) > 1) {
+    #   stop('non-conformal coordinate names', call. = FALSE)
+    # }
+    
+    
+    # spatial points require some more effort when spatial data are missing
+    o.1.sp <- spc.list[[1]]$sp
+    
+    # missing spatial data
+    if(ncol(coordinates(o.1.sp)) == 1) {
+      o.sp <- o.1.sp # copy the first filler
+    } else {
+      # not missing spatial data
+      # 2015-12-18: added call to specific function: "sp::rbind.SpatialPoints"
+      o.sp <- do.call("rbind.SpatialPoints", o.sp) 
+    }
+    
   } else {
-    # not missing spatial data
-    # 2015-12-18: added call to specific function: "sp::rbind.SpatialPoints"
-    o.sp <- do.call("rbind.SpatialPoints", o.sp) 
+    # default NULL SpatialPoints
+    o.sp <- new('SpatialPoints')
   }
   
   
-  ## make SPC and return
-  res <- SoilProfileCollection(idcol=o.idname[[1]], depthcols=o.hz.depths[[1]], metadata=o.m[[1]], horizons=o.h, site=o.s, sp=o.sp, diagnostic=o.d)
+  ## sanity check: profile IDs should be unique
+  if(length(o.s[[new.pID]]) != length(unique(o.s[[new.pID]]))) {
+    stop('non-unique profile IDs detected')
+  }
+  
+  ## make SPC from pieces
+  res <- SoilProfileCollection(idcol=new.pID, depthcols=new.hzd, metadata=new.metadata, horizons=o.h, site=o.s, sp=o.sp, diagnostic=o.d)
   
   ## reset horizon IDs
   hzID(res) <- 1:nrow(res)
-  
-  
-  ## final sanity checks
-  if(length(profile_id(res)) != length(site(res)[[idname(res)]]))
-    stop("SPC object corruption. This shouldn't happen and will be fixed in aqp 2.0", call. = FALSE)
-  
-  if(! all.equal(profile_id(res), site(res)[[idname(res)]]))
-    stop("SPC object corruption. This shouldn't happen and will be fixed in aqp 2.0", call. = FALSE)
   
   return(res)
 }
--- tests/testthat/test-SPC-union.R
+++ tests/testthat/test-SPC-union.R
@@ -8,8 +8,8 @@ site(sp1) <- ~ group
 sp1$x <- seq(-119, -120, length.out = length(sp1))
 sp1$y <- seq(38, 39, length.out = length(sp1))
 
-coordinates(sp1) <- ~ x + y
-
+sp::coordinates(sp1) <- ~ x + y
+sp::proj4string(sp1) <- '+proj=longlat +datum=WGS84'
 
 test_that("basic union tests", {
   
@@ -43,7 +43,90 @@ test_that("basic union tests", {
   # diagnostic features
   expect_equal(diagnostic_hz(z)[[idname(z)]], 'P001-copy')
   
-  # TODO: spatial data checks
 })
 
+
+
+
+test_that("non-conformal union tests", {
+  
+  # random data
+  ids <- sprintf("%02d", 1:5)
+  x <- plyr::ldply(ids, random_profile, n=c(6, 7, 8), n_prop=1, method='LPP', 
+             lpp.a=5, lpp.b=15, lpp.d=5, lpp.e=5, lpp.u=25)
+  
+  depths(x) <- id ~ top + bottom
+  
+  # more random data
+  ids <- sprintf("%s", letters[1:5])
+  y <- plyr::ldply(ids, random_profile, n=c(6, 7, 8), n_prop=4, method='LPP', 
+             lpp.a=5, lpp.b=15, lpp.d=5, lpp.e=5, lpp.u=25)
+  
+  # alter ID, top, bottom column names
+  y$pID <- y$id
+  y$hztop <- y$top
+  y$hzbot <- y$bottom
+  
+  y$id <- NULL
+  y$top <- NULL
+  y$bottom <- NULL
+  
+  depths(y) <- pID ~ hztop + hzbot
+  
+  # very different data
+  data('mineralKing', package = 'soilDB')
+  
+  # alter depth units
+  depth_units(y) <- 'in'
+  
+  # should throw an error
+  expect_error(union(list(x, y)), "inconsistent depth units")
+  
+  # reset depth units
+  depth_units(y) <- 'cm'
+  
+  # attempt union
+  z <- union(list(x, y, mineralKing))
+  
+  # there should be a total of 20 profiles in the union
+  expect_equal(sum(sapply(list(x, y, mineralKing), length)), 20)
+  expect_equal(length(z), sum(sapply(list(x, y, mineralKing), length)))
+  
+})
+
+
+test_that("union with non-conformal spatial data", {
+  
+  # test data
+  x <- sp1
+  y <- sp1
+  z <- sp1
+  
+  # alter CRS, this generates an sp warning
+  expect_warning(sp::proj4string(y) <- '+proj=utm +zone=10 +datum=NAD83')
+  
+  # should throw an error
+  expect_error(union(list(x,y)), 'inconsistent CRS')
+  
+  # make IDs unique
+  profile_id(y) <- sprintf("%s-copy", profile_id(y))
+  profile_id(z) <- sprintf("%s-copy-copy", profile_id(z))
+  
+  # remove sp data for one
+  z@sp <- new('SpatialPoints')
+  
+  # remove all CRS
+  sp::proj4string(x) <- ''
+  sp::proj4string(y) <- ''
+  sp::proj4string(z) <- ''
+  
+  # should throw an error
+  expect_error(union(list(x, y, z)), 'non-conformal point geometry')
+  
+  # drop spatial data and no error
+  res <- union(list(x, y, z), drop.spatial = TRUE)
+  expect_equivalent(class(res), 'SoilProfileCollection')
+  
+  ## TODO: different coordinate names
+})
 
