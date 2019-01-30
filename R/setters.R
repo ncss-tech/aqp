df0b3c504d6fb8bb03d3a54c5fa9dcc1bd32c2aa
Andrew Brown <agbrown@ucdavis.edu>
2019-01-29 20:25:51 -0800

    Revert "Merge remote-tracking branch 'refs/remotes/origin/andrew'"
    These changes were not ready for merging into `master` branch. Possibly
    will be added soon
    
    This reverts commit 9f066565cf637ace388d72b81dcc3016307fdc7d, reversing
    changes made to 208bb6188873c3407cfee4e10b84e04e9485fe7a.

--- R/setters.R
+++ R/setters.R
@@ -508,41 +508,14 @@ setReplaceMethod("horizons", "SoilProfileCollection",
   ##
   # testing the number of rows of the horizon data
   # if (nrow(value) != nrow(object))
-  # stop("inconsistent number of rows")
-  
-  # improved test of IDs for horizon setter
-  
-  # if profile_id attribute is NOT in the new horizon data
-  if(!(idname(object) %in% names(value))) {
-    # try to create one based on the old horizon-site pairing
-    
-    # check if all old horizon IDs are in rowname of input, and create hzidname column if they are
-    if(all(hzID(loafercreek) %in% rownames(value))) {
-      if(!hzidname(object) %in% names(value)) {
-        value[[hzidname(object)]] <- rownames(value)
-      }
-    }
-    
-    # we can't create one if there isn't at least a horizon id in the new values
-    if(!hzidname(object) %in% names(value))
-      stop("there is no matching horizon ID column in replacement", call.=FALSE)   
-    
-    # create lookup table for profile ID based on horizon ID in original horizon data
-    lut <- object@horizons[[idname(object)]]
-    names(lut) <- object@horizons[[hzidname(object)]]
-    
-    # only create the profile_id column in the horizon table if we can match all the horizon ids to a profile
-    can.lookup <- value[[hzidname(object)]] %in% names(lut)
-    if(all(can.lookup)) {
-      value[[idname(object)]] <- lut[can.lookup ]
-    } else {
-      if(length(setdiff(unique(as.character(value[[hzidname(object)]])), hzID(object))) > 0)
-        stop("there are horizon IDs in the replacement that do not exist in the original data", call.=FALSE)
-    }
-  }
+	  # stop("inconsistent number of rows")
+
+  # basic test of ids:
+  if(!idname(object) %in% names(value)) # is there a matching ID column in the replacement?
+  	stop("there is no matching ID column in replacement", call.=FALSE)
 
   if(length(setdiff(unique(as.character(value[[idname(object)]])), profile_id(object))) > 0)
-  	stop("there are profile IDs in the replacement that do not exist in the original data", call.=FALSE)
+  	stop("there are IDs in the replacement that do not exist in the original data", call.=FALSE)
 
   ##
   ## 2017-01-05: holy shit, why are we re-ordering the horizon data? 
