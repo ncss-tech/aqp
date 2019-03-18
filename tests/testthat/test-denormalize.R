0a9323639fa47b603018cf513f536b9a3a6dd2c7
Andrew Gene Brown <andrew.g.brown@ca.usda.gov>
2019-03-18 12:27:09 -0700

    tweak denormalize test

--- tests/testthat/test-denormalize.R
+++ tests/testthat/test-denormalize.R
@@ -8,14 +8,14 @@ depths(sp1) <- ~ top + bottom
 # create random site-level attribute `sitevar` with a binary (0/1) outcome
 sp1$sitevar <- round(runif(length(sp1)))
 
+test_that("denormalize result is 1:1 with horizons", {
 # use denormalize() to create a mirror of sitevar in the horizon table
 # name the attribute something different (e.g. `hz.sitevar`) to prevent collision with the site attribute
 # the attributes can have the same name but you will then need site() or horizons() to access explicitly
-sp1$hz.sitevar <- denormalize(sp1, 'sitevar')
-
-# compare number of profiles to number of sitevar assignments
-# and number of horizons to number of values in denormalize result
-test_that("denormalize result is 1:1 with horizons", {
-  expect_equal(length(sp1), sum(table(sp1$sitevar)))  # check that the input is 1:1 with site
-  expect_equal(nrow(sp1), sum(table(sp1$hz.sitevar))) # check that the output is 1:1 with horizon
+  sp1.hz.sitevar <- denormalize(sp1, 'sitevar')
+  
+# compare number of horizons to number of values in denormalize result
+  expect_equal(nrow(sp1), length(sp1.hz.sitevar)) # check that the output is 1:1 with horizon
+  
+  sp1$hz.sitevar <- sp1.hz.sitevar
 })
