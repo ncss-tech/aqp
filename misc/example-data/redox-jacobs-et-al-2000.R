aed6b6dfd5ab81f34bc9afd5308ce7b2d744c050
Dylan Beaudette <debeaudette@ucdavis.edu>
2019-09-23 12:00:36 -0700

    house cleaning, moving example data prep to new folder

---
+++ misc/example-data/redox-jacobs-et-al-2000.R
@@ -0,0 +1,12 @@
+# https://dl.sciencesocieties.org/publications/sssaj/articles/66/1/315
+
+
+h <- read.table('redox-jacobs-et-al-2000-hz.txt', header = TRUE, sep='\t', quote = '', stringsAsFactors = FALSE)
+str(h)
+
+s <- read.table('redox-jacobs-et-al-2000-site.txt', header = TRUE, sep='|', quote = '', stringsAsFactors = FALSE)
+str(s)
+
+
+
+
