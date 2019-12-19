de6fd774abcd7b4fe68424c7268eb332c175b5f9
Dylan Beaudette <debeaudette@ucdavis.edu>
2019-12-19 13:31:38 -0800

    evalMissingData gains an argument and functionality
    this will support a new take on the profile completeness index

--- R/evalMissingData.R
+++ R/evalMissingData.R
@@ -1,13 +1,12 @@
 
-## TODO: this needs checking / documentation
-
 ## missing data fraction computed on a thickness basis
 ## this is meant to be run on a single profile at a time
 # x: single SPC
 # v: variables to consider
 # n: horizon designations
 # p: inverted pattern matching non-soil horizons
-.getMissingDataFraction <- function(x, v, n, p) {
+# m: method, 'relative' or 'absolute'
+.getMissingDataFraction <- function(x, v, n, p, m) {
   # get horizons
   h <- horizons(x)
   # get top/bottom
@@ -21,10 +20,22 @@
   # extract named columns, but only those from @horizons
   v <- as.vector(na.omit(match(v, horizonNames(x))))
   d <- h[soil.hz, v]
+  
+  # mask out non-soil horizons
   hz.thick <- hz.thick[soil.hz]
+  
+  # index hz with full data population
   hz.with.data <- which(complete.cases(d))
-  # compute fraction of data based on thickness
-  res <- sum(hz.thick[hz.with.data], na.rm=TRUE) / sum(hz.thick, na.rm = TRUE)
+  
+  # compute fraction of data based on:
+  # relative thickness
+  # absolute thickness
+  total <- switch(m,
+                  'relative' = sum(hz.thick, na.rm = TRUE),
+                  'absolute' = 1
+  )
+     
+  res <- sum(hz.thick[hz.with.data], na.rm=TRUE) / total
   
   return(res)
 }
@@ -35,9 +46,28 @@
 # vars: variables to consider
 # name: horizon designations
 # p: inverted pattern matching non-soil horizons
-evalMissingData <- function(x, vars, name='hzname', p='Cr|R|Cd') {
+# method: 'relative' or 'absolute' reporting
+evalMissingData <- function(x, vars, name='hzname', p='Cr|R|Cd', method='relative') {
+  # sanity check
+  if(! method %in% c('relative', 'absolute')) {
+    stop("method should be one of 'relative' or 'absolute'", call. = FALSE)
+  }
+  
+  hn <- horizonNames(x)
+  sdv <- setdiff(vars, hn)
+  
+  # check for bad hzname
+  if(! name %in% hn) {
+    stop(sprintf('`%s` not a horizon level attribute', name), call. = FALSE)
+  }
+  
+  # check for bad hz attr
+  if(length(sdv > 1)) {
+    stop(sprintf('`%s` not horizon level attribute(s)', paste(sdv, collapse = ',')), call. = FALSE)
+  }
+  
   # apply missing data fraction eval to each profile
-  md <- profileApply(x, .getMissingDataFraction, v=vars, n=name, p=p)
+  md <- profileApply(x, .getMissingDataFraction, v=vars, n=name, p=p, m=method)
   return(md)
 }
 
