edaa7c264e42697ac97e018043ecf3a696be8eb5
Dylan Beaudette <debeaudette@ucdavis.edu>
2020-01-07 11:53:28 -0800

    safely rebuild SPCs pre-horizon IDs

--- R/rebuildSPC.R
+++ R/rebuildSPC.R
@@ -13,19 +13,24 @@ rebuildSPC <- function(x) {
   fm <- as.formula(sprintf("%s ~ %s + %s", x.list$idcol, x.list$depthcols[1], x.list$depthcols[2]))
   depths(res) <- fm
   
-  # preserve original hzidname (default set by depths())
-  hzidname(res) <- x.list$hzidcol
+  # preserve original hzidname
+  # this is missing in old SPC objects
+  if(!is.null(x.list$hzidcol)) {
+    hzidname(res) <- x.list$hzidcol
+  }
   
   # add additional pieces
   metadata(res) <- x.list$metadata
   site(res) <- x.list$site
   
-  # @diagnostic and @restructions may be missing if `x` is very old
+  # @diagnostic and may be missing if `x` is very old
   # replace with empty data.frame so that setters do not error
   if(is.null(x.list$diagnostic)) {
     x.list$diagnostic <- data.frame()
   }
   
+  # @restructions may be missing if `x` is very old
+  # replace with empty data.frame so that setters do not error
   if(is.null(x.list$restrictions)) {
     x.list$restrictions <- data.frame()
   }
