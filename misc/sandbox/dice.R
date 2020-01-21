e0fb25b21dc570973334c7f3919cac114d07ebbf
Dylan Beaudette <debeaudette@ucdavis.edu>
2020-01-21 11:54:34 -0800

    mostly complete re-write of core functionality of slice()

--- misc/sandbox/dice.R
+++ misc/sandbox/dice.R
@@ -2,7 +2,7 @@
 library(aqp)
 library(data.table)
 
-d <- lapply(1:50000, random_profile, n=c(6, 7, 8), n_prop=5, method='LPP')
+d <- lapply(1:1000, random_profile, n=c(6, 7, 8), n_prop=5, method='LPP')
 d <- do.call('rbind', d)
 d$id <- as.character(d$id)
 depths(d) <- id ~ top + bottom
@@ -12,45 +12,8 @@ d$group <- factor(sample(letters[1:10], size=length(d), replace=TRUE))
 # https://stackoverflow.com/questions/15170741/how-does-one-do-a-full-join-using-data-table
 # 
 
-## TODO convert to profileApply() invocation
-## general-purpose hz depth logic check + fix
-checkHzDepthLogic <- function(x) {
-  
-  ## profileApply()
-  
-  h <- horizons(x)
-  
-  # bottom depth < top depth?
-  idx.1 <- which(h$bottom < h$top)
-  
-  # bottom depth == top depth
-  idx.2 <- which(h$top == h$bottom)
-  
-  # NA depths
-  idx.3 <- which(is.na(h$top) | is.na(h$bottom))
-  
-  # bottom != next top
-  idx.4 <- which(h$top[-1] != h$bottom[-nrow(h)])
-  
-  if(length(idx.1) > 0 | length(idx.2) > 0 | length(idx.3 > 3)) {
-    warning('depth logic errors present')
-  }
-  
-  
-  ## TODO: return NULL if no errors
-  # convert horizon indexes into profile_ids
-  res <- list(
-    depthLogicError=h[[idname(x)]][idx.1], 
-    sameDepth=h[[idname(x)]][idx.2], 
-    missingDepth=h[[idname(x)]][idx.3],
-    overlapOrGap=h[[idname(x)]][idx.4]
-    )
-  
-  return(res)
-}
 
-
-# simpler, faster version of slice via FULL JOIN
+# simpler, faster version of slice via data.table / FULL JOIN
 # less robust to errors than current slice()
 dice <- function(x) {
   
@@ -66,11 +29,12 @@ dice <- function(x) {
   ## extract pieces
   h <- horizons(x)
   idn <- idname(x)
+  hzidn <- hzidname(x)
   htb <- horizonDepths(x)
   
   ## `h` will eventually be a data.table object
   h <- as.data.table(h)
-  setkeyv(h, hzidname(x))
+  setkeyv(h, hzidn)
   
   ## mapply() call takes 1/2 of total time
   ## consider custom function
@@ -80,25 +44,24 @@ dice <- function(x) {
   bottoms <- tops + 1
   
   # expand slice IDs (horizon IDs)
-  sliceIDs <- rep(h[[hzidname(x)]], times=h[[htb[2]]] - h[[htb[1]]])
+  sliceIDs <- rep(h[[hzidn]], times=h[[htb[2]]] - h[[htb[1]]])
   
   ## TODO: this will eventually be a data.table object
   # assemble for JOIN
-  s <- data.frame(.sliceID=sliceIDs, .sliceTop=tops, .sliceBottom=bottoms, stringsAsFactors = FALSE)
+  s <- data.frame(sliceID=sliceIDs, .sliceTop=tops, .sliceBottom=bottoms, stringsAsFactors = FALSE)
   # re-name for simpler JOIN
-  names(s)[1] <- hzidname(x)
+  names(s)[1] <- hzidn
   # init / index data.table
   s <- as.data.table(s)
-  setkeyv(s, hzidname(x))
+  setkeyv(s, hzidn)
   
-  ## TODO: utilize fast joins via data.table objects with keys SET
-  # full JOIN
-  res <- merge.data.table(h, s, by=hzidname(x), all=TRUE, sort=FALSE)
+  # FULL JOIN via fast data.table compiled code
+  res <- merge.data.table(h, s, by=hzidn, all=TRUE, sort=FALSE)
   
-  # copy old horizon IDs
-  res[['.oldHzID']] <- res[[hzidname(x)]]
+  ## TODO: update to := syntax, but how does it work with with variables?
+  # https://cran.r-project.org/web/packages/data.table/vignettes/datatable-reference-semantics.html
   # init unique horizon IDs
-  res[[hzidname(x)]] <- 1:nrow(res)
+  res[['sliceID']] <- 1:nrow(res)
   
   # copy old depths
   res[['.oldTop']] <- res[[htb[1]]]
@@ -112,29 +75,35 @@ dice <- function(x) {
   res[['.sliceTop']] <- NULL
   res[['.sliceBottom']] <- NULL
   
-  # pack back into SPC
+  # pack back into SPC by replacement of @horizons
+  # otherwise the setter will attempt to join the data on hzidname
   res <- as.data.frame(res)
-  horizons(x) <- res
+  slot(x, 'horizons') <- res
+  
+  # switch horizon ID to slice ID
+  hzidname(x) <- 'sliceID'
   
   return(x)
 }
 
 
-z <- dice(d[1:20, ])
+z <- dice(d[1:2, ])
 
-plot(z, color='p2')
+par(mar=c(0,1,3,1))
+plotSPC(z, color='hzID', name='', divide.hz = TRUE)
 
 # 87 seconds: current slice()
 system.time(s <- slice(d, 0:100 ~ .))
 
 # 39 seconds: 2x mapply, merge.data.frame
-# 12 seconds: 1x mapply, merge.data.table
+# 10 seconds: 1x mapply, merge.data.table
 system.time(s <- dice(d))
-
 
 
 z <- d[1, ]
 z$bottom[2] <- NA
+
+ck <- checkHzDepthLogic(z)
 
 dice(z)
 
