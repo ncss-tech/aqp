afb627ca592a6a7d40ea08f3d344618558a3b492
Andrew Gene Brown <andrew.g.brown@ca.usda.gov>
2020-02-09 22:44:11 -0800

    glomApply() is this useful?

---
+++ R/glomApply.R
@@ -0,0 +1,17 @@
+#' @title Subset an SPC by applying glom to each profile
+#' @name glomApply
+#' @aliases glomApply,SoilProfileCollection-method
+#' @description \code{glomApply()} is a function used for subsetting SoilProfileCollections. A function \code{.fun} is supplied which is expected to return a top and bottom depth when supplied with a single-profile SPC and ... (any additional parameters). This numeric vector of length two is used by \code{glom()} to produce a subset SPC that is just the horizons overlapping the specified interval.
+#' @param object A SoilProfileCollection
+#' @param ... A set of comma-delimited R expressions that resolve to a transformation to be applied to a single profile e.g \code{glomApply(hzdept = max(hzdept) - hzdept)}
+#' @return A SoilProfileCollection.
+#' @author Andrew G. Brown.
+#' 
+#' @rdname glomApply
+#' @export glomApply
+glomApply <- function(object, .fun=NULL, ..., chunk.size = 100) {
+  aqp::union(profileApply(object, function(p) {
+    dep <- .fun(p, ...)
+    return(glom(p, dep[1], dep[2]))
+  }, simplify = FALSE, chunk.size = chunk.size))
+}
