b487b03bab4e43a5b89665fcaa275222c45d1cb2
Andrew Gene Brown <andrew.g.brown@ca.usda.gov>
2020-03-17 02:52:00 -0700

    fixes for explainPlotSPC, correction of relative.pos logic for buffer.spc>1

--- R/plotSPC.R
+++ R/plotSPC.R
@@ -74,7 +74,9 @@ plotSPC <- function(x, color='soil_color', width=0.2, name=NULL, label=idname(x)
     new.order[(buffer.spc+1):(buffer.spc+length(plot.order))] <- plot.order+buffer.spc
     plot.order <- new.order
     
-    relative.pos <- c(buf.i, max(buf.i) + relative.pos, max(relative.pos) + buf.i + 1)
+    relative.pos <- c(buf.i, 
+                      max(buf.i) + relative.pos, 
+                      (max(relative.pos) + max(buf.i)) + buf.i)
   } 
   
   # padding along x-axis, prevents crowding beyond last profile
@@ -130,7 +132,8 @@ plotSPC <- function(x, color='soil_color', width=0.2, name=NULL, label=idname(x)
               'max.depth'=max.depth, 
               'n'=n,
               'extra_x_space'=extra_x_space,
-              'extra_y_space'=extra_y_space)
+              'extra_y_space'=extra_y_space,
+              'buffer.spc'=buffer.spc)
   
   assign('last_spc_plot', lsp, envir=aqp.env)
   
