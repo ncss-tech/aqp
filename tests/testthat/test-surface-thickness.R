b83d191ab06ee2c53a86a45815cbc554ddcae0b2
Andrew Gene Brown <andrew.g.brown@ca.usda.gov>
2020-05-18 13:16:50 -0700

    testthat in getSurfaceHorizonDepth contiguous and non-contiguous matches below a non-match are ignored (#132)

--- tests/testthat/test-surface-thickness.R
+++ tests/testthat/test-surface-thickness.R
@@ -12,13 +12,18 @@ attr <- 'prop' # clay contents %
 q <- sp1[2]
 
 test_that("getSurfaceHorizonDepth", {
-  #all horizons containing A
+  # all horizons containing A (n=4 from top)
   t1 <- getSurfaceHorizonDepth(p, pattern = 'A', hzdesgn = 'name')
-  expect_equal(t1, 57)
+  expect_equal(t1, horizons(p)[4, 'bottom'])
   
-  #all horizons that start with A and have a number from _2_ to 9
+  # all horizons that start with A and have a number from _2_ to 9 (n=0)
   t4 <- getSurfaceHorizonDepth(p, pattern = '^A[2-9]', hzdesgn = 'name')
   expect_equal(t4, 0)
+  
+  # contiguous and non-contiguous matches below a non-match are ignored (n=1)
+  p$mollic_color <- c("d","l","d","l","d","d")
+  t6 <- getSurfaceHorizonDepth(p, pattern="d", hzdesgn="mollic_color")
+  expect_equal(t6, horizons(p)[1, 'bottom'])
 })
 
 test_that("getPlowLayerDepth()", {
