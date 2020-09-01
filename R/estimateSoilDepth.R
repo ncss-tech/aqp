c07c97723cc3d4b826b5d6c19777caa27efe6588
Dylan Beaudette <debeaudette@ucdavis.edu>
2020-08-31 23:43:02 -0700

    cleanup of estimateSoilDepth, likely breaking other things as arguments have been modified #27

--- R/estimateSoilDepth.R
+++ R/estimateSoilDepth.R
@@ -1,29 +1,100 @@
 
-# get soil depth based on morphology
-estimateSoilDepth <- function(f, name='hzname', top='hzdept', bottom='hzdepb', p='Cr|R|Cd', no.contact.depth=NULL, no.contact.assigned=NULL) {
+#' @title Estimate Soil Depth
+#' @description Estimate the soil depth of a single profile within a SoilProfileCollection object. This function would typically be called by \code{\link{profileApply}}.
+#' 
+#' @param f SoilProfileCollection object of length 1, e.g. a single profile
+#' @param name name of the column that contains horizon designations
+#' @param p REGEX pattern for determining "contact", or depth to some morphologic feature (e.g. `Bt`)
+#' @param selection function applied in the presence of multiple matching horizons: \code{min} (default), \code{max}, \code{mean}, etc.
+#' @param no.contact.depth in the absense of contact matching \code{p}, a depth at which we can assume a standard depth-to-contact
+#' @param no.contact.assigned value assigned when no contact is encountered at or below \code{no.contact.depth}
+#' 
+#' @return single value representing the depth to `contact` or \code{no.contact.assigned}
+#' 
+#' @author D.E. Beaudette and J.M. Skovlin
+#' 
+#' @seealso \code{\link{getSoilDepthClass}}, \code{\link{profileApply}}
+#' 
+#' @keywords manip
+#' 
+#' @examples 
+#' data(sp1)
+#' depths(sp1) <- id ~ top + bottom
+#' 
+#' # apply to each profile in a collection, and save as site-level attribute
+#' sp1$depth <- profileApply(sp1, estimateSoilDepth, name='name')
+#' 
+#' # this function can be used to "find" depth to any feature 
+#' # that can be defined via REGEX pattern matching on the horizon name
+#' # for example, locate the depth to the top "Bt" horizon
+#' # returning NA when there is no match
+#' sp1$top_Bt <- profileApply(
+#'   sp1, estimateSoilDepth, 
+#'   name='name', 
+#'   p='Bt', 
+#'   no.contact.depth=0, 
+#'   no.contact.assigned=NA
+#' )
+#' 
+#' # reduced margins
+#' par(mar=c(1,1,1,2))
+#' # adjust default y-offset and depth scaling for following examples
+#' plotSPC(sp1, y.offset=10, scaling.factor=0.5)
+#' 
+#' # get plotting parameters for profile widths and depth scaling factors
+#' lsp <- get("last_spc_plot", envir = aqp.env)
+#' 
+#' # positions on x-axis, same for both depth and top "Bt" horizon
+#' x.positions <- (1:length(sp1)) - lsp$width
+#' 
+#' # annotate contact with unicode right-arrow
+#' # y-position is adjusted based on plot y-offset and scaling factor
+#' y.positions <- lsp$y.offset + (sp1$depth * lsp$scaling.factor)
+#' text(x.positions, y.positions, '\u2192', col='red', adj=1, cex=1.25, lwd=2)
+#' 
+#' # annotate top "Bt" depth with unicode right-arrow
+#' # y-position is adjusted based on plot y-offset and scaling factor
+#' y.positions <- lsp$y.offset + (sp1$top_Bt * lsp$scaling.factor)
+#' text(x.positions, y.positions, '\u2192', col='blue', adj=1, cex=1.25, lwd=2)
+#' 
+#' 
+#' \dontrun{
+#'   # sample data
+#'   data(gopheridge, package='soilDB')
+#'   
+#'   # run on a single profile
+#'   estimateSoilDepth(gopheridge[1, ])
+#'   
+#'   # apply to an entire collection
+#'   profileApply(gopheridge, estimateSoilDepth)
+#' }
+
+
+estimateSoilDepth <- function(f, name='hzname', p='Cr|R|Cd', selection = min, no.contact.depth=NULL, no.contact.assigned=NULL) {
 
   # sanity check: this function will only operate on an SPC
-  if(!inherits(f, 'SoilProfileCollection') | length(f) != 1)
-    stop('`f` must be a SoilProfileCollection containing one profile')
-
-  depthcols <- horizonDepths(f)
-
-  # ease removal of attribute name arguments -- deprecate them later
-  # for now, just fix em if the defaults dont match the depthcols slot
-  if(any(!(c(top, bottom) %in% horizonNames(f)))) {
-    top <- depthcols[1]
-    bottom <- depthcols[2]
+  if(! inherits(f, 'SoilProfileCollection')) {
+    stop('`f` must be a SoilProfileCollection containing one profile', call. = FALSE)
   }
-
-  # sanity check: this function works on a single soil profile
-  if(length(f) > 1)
+  
+  # sanity check: this function works on SPC with length 1
+  if(length(f) > 1) {
     stop('`f` can contain only one profile, see manual page for details')
+  }
+    
+  
+  # use SPC depth column names
+  depthcols <- horizonDepths(f)
+  top <- depthcols[1]
+  bottom <- depthcols[2]
+  
+  ## TODO: guessing a horizon name isn't a good idea: this function requires that one be specified
 
   # if name is not in horizons, look if it is set in hzdesgncol
   hznames <- horizonNames(f)
 
   # if the user has not specified a column containing horizon designations
-  if(!name %in% hznames) {
+  if(! name %in% hznames) {
     name <- guessHzDesgnName(f)
     if(is.na(name)) {
       stop("soil depth estimation relies on a column containing horizon designations", call.=FALSE)
@@ -37,27 +108,30 @@ estimateSoilDepth <- function(f, name='hzname', top='hzdept', bottom='hzdepb', p
   contact.idx <- grep(p, h[[name]], ignore.case=TRUE)
   # everything else
   no.contact.idx <- grep(p, h[[name]], ignore.case=TRUE, invert=TRUE)
-
+  
+  ## TODO: this is really hard to follow, simplify / test
+  
   # no contact defined, use deepest hz bottom depth
   if(length(contact.idx) < 1) {
     d <- max(h[[bottom]][no.contact.idx], na.rm=TRUE)
 
     # is there a user-specified depth at which we assume a standard depth?
-    if(!missing(no.contact.depth) & !missing(no.contact.assigned)) {
-      if(d >= no.contact.depth & !is.null(no.contact.assigned))
+    if(! is.null(no.contact.depth) & ! is.null(no.contact.assigned)) {
+      if(d >= no.contact.depth & !is.null(no.contact.assigned)) {
         res <- no.contact.assigned
-      else
+      } else {
         res <- d
-    }
-
-    # otherwise use depth of deepest horizon
-    else
+      }
+    } else {
+      # otherwise use depth of deepest horizon
       res <- d
+    }
+      
+  } else {
+    # contact defined
+    res <- selection(h[[top]][contact.idx], na.rm=TRUE)
   }
-
-  # contact defined
-  else
-    res <- min(h[[top]][contact.idx], na.rm=TRUE)
+    
 
   return(res)
 }
