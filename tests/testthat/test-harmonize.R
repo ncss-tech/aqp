6b086267d56c81722844d62ee29ff09a2940aa09
Andrew G. Brown <andrew.g.brown@ca.usda.gov>
2020-09-04 18:19:54 -0700

    test: harmonize is immune to column name order #164

--- tests/testthat/test-harmonize.R
+++ tests/testthat/test-harmonize.R
@@ -3,6 +3,10 @@ context("profile-level harmonization of horizon properties")
 set.seed(1)
 spc <- aqp::union(lapply(1:10, random_profile, SPC = TRUE))
 
+# this method should be "immune" to column name ordering -- so shuffle them
+spc.nhzcol <- length(horizonNames(spc))
+replaceHorizons(spc) <- horizons(spc)[,sample(1:spc.nhzcol, spc.nhzcol)]
+
 test_that("basic profile denormalization of \"range\" for one property", {
   expect_silent({
      h1 <- harmonize(spc, x.names = list(prop = c(q05 = "p1", q50 = "p2", q95 = "p3")), keep.cols = "p5")
@@ -13,9 +17,6 @@ test_that("basic profile denormalization of \"range\" for one property", {
   expect_equal(nrow(h1), 141)
   expect_equal(round(mean(h1$prop),6), -0.390285)
   
-  # original names are gone
-  expect_true(!any(c("p1","p2","p3") %in% horizonNames(h1)))
-  
-  # harmonized name + keep columns are presnt
-  expect_true(all(c("prop","p5") %in% horizonNames(h1)))
+  # original names are gone harmonized name + keep columns are present and in order
+  expect_true(all(c("id","top","bottom","prop","p5","hzID") == horizonNames(h1)))
 })
