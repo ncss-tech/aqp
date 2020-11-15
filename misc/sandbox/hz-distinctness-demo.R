1db8383fccd8c2428ad40f469521bccb90ded4d8
Dylan Beaudette <debeaudette@ucdavis.edu>
2020-11-14 23:07:02 -0800

    Update hz-distinctness-demo.R

--- misc/sandbox/hz-distinctness-demo.R
+++ misc/sandbox/hz-distinctness-demo.R
@@ -2,30 +2,38 @@ library(aqp)
 library(soilDB)
 
 
-x <- fetchOSD(c('leon', 'musick', 'clarksville', 'sierra', 'pardee', 'amador', 'lucy', 'dylan', 'tristan', 'pierre', 'drummer', 'zook'))
+osds <- fetchOSD(c('leon', 'musick', 'clarksville', 'sierra', 'pardee', 'amador', 'lucy', 'dylan', 'tristan', 'pierre', 'drummer', 'zook'))
 
 # encode horizon boundarydistinctness via vertical offset
-x$hd <- hzDistinctnessCodeToOffset(
-  x$distinctness, 
+osds$hd <- hzDistinctnessCodeToOffset(
+  osds$distinctness, 
   codes=c('very abrupt', 'abrupt', 'clear', 'gradual', 'diffuse')
   )
 
 # encode horizon boundary topography via vertical offset
-x$hzto <- hzTopographyCodeToOffset(
-  x$topography, 
+osds$hzto <- hzTopographyCodeToOffset(
+  osds$topography, 
   codes = c('smooth', 'wavy', 'irregular', 'broken')
 )
 
-site(x)$code <- 'OSD'
+site(osds)$code <- 'OSD'
 
+osds$bnd.code <- sprintf(
+  "%s/%s",
+  substr(osds$distinctness, 1, 1),
+  substr(osds$topography, 1, 1)
+)
+
+osds$bnd.code <- gsub('NA/NA', '', osds$bnd.code)
 
 # ok
 par(mar = c(0, 0, 0, 1), bg = 'black', fg = 'white')
-plotSPC(x, width = 0.3, hz.distinctness.offset = 'hd', hz.topography.offset = 'hzto', cex.names = 0.55) 
+plotSPC(osds, width = 0.3, hz.distinctness.offset = 'hd', hz.topography.offset = 'hzto', cex.names = 0.55) 
 
+plotSPC(osds, width = 0.3, hz.distinctness.offset = 'hd', hz.topography.offset = 'hzto', cex.names = 0.66, name = 'bnd.code') 
 
 # keep a single OSD for the demo
-x <- filter(x, id == 'LEON')
+x <- filter(osds, id == 'LEON')
 
 # safely duplicate these data 6 times
 # --> new IDs generated, old IDs saved into .oldID
@@ -98,3 +106,32 @@ title('Horizon Boundary Distinctness', line = 0)
 
 
 
+
+## combinations
+
+x <- filter(osds, id == 'ZOOK')
+
+g <- expand.grid(
+  topography = c('S', 'W', 'I', 'B'),
+  distinctness = c('V', 'A', 'C', 'G', 'D')
+  
+)
+
+l <- list()
+s <- duplicate(x, times = nrow(g))
+for(i in 1:nrow(g)) {
+  # current profile
+  ss <- s[i, ]
+  # set for all horizons
+  horizons(ss)$hd <- hzDistinctnessCodeToOffset(g$distinctness[i])
+  horizons(ss)$ht <- hzTopographyCodeToOffset(g$topography[i])
+  l[[i]] <- ss  
+}
+
+s <- combine(l)
+s$code <- sprintf("%s/%s", g$distinctness, g$topography)
+
+par(mar=c(0,0,1,1), bg = 'black', fg = 'white')
+plotSPC(s, width=0.33, print.id=TRUE, hz.distinctness.offset = 'hd', hz.topography.offset = 'ht', label='code', cex.names=0.8, name = NA, color = NA, default.color = 'black')
+title('Horizon Boundary Types', line = 0, col.main = 'white')
+abline(h = x$top, col = 'green', lty = 3)
