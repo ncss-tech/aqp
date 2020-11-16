3a40bfdb2036d48c3fea136e433d08f2124c06a5
Dylan Beaudette <debeaudette@ucdavis.edu>
2020-11-16 14:37:38 -0800

    tinkering

--- misc/sandbox/hz-distinctness-demo.R
+++ misc/sandbox/hz-distinctness-demo.R
@@ -16,21 +16,38 @@ osds$hzto <- hzTopographyCodeToOffset(
   codes = c('smooth', 'wavy', 'irregular', 'broken')
 )
 
+# also encode horizon boundary topography las line type
+osds$hzto.lty <- hzTopographyCodeToLineType(
+  osds$topography,
+  codes = c('smooth', 'wavy', 'irregular', 'broken')
+)
+
 site(osds)$code <- 'OSD'
 
 osds$bnd.code <- sprintf(
-  "%s/%s",
+  "%s%s",
   substr(osds$distinctness, 1, 1),
   substr(osds$topography, 1, 1)
 )
 
-osds$bnd.code <- gsub('NA/NA', '', osds$bnd.code)
+osds$bnd.code <- gsub('NANA', '', osds$bnd.code)
 
 # ok
 par(mar = c(0, 0, 0, 1), bg = 'black', fg = 'white')
-plotSPC(osds, width = 0.3, hz.distinctness.offset = 'hd', hz.topography.offset = 'hzto', cex.names = 0.55) 
+plotSPC(osds, width = 0.3, hz.distinctness.offset = 'hd', hz.topography.offset = 'hzto', cex.id = 0.66, cex.names = 0.66) 
 
-plotSPC(osds, width = 0.3, hz.distinctness.offset = 'hd', hz.topography.offset = 'hzto', cex.names = 0.66, name = 'bnd.code') 
+plotSPC(osds, width = 0.3, hz.distinctness.offset = 'hd', hz.topography.offset = 'hzto', cex.id = 0.66, cex.names = 0.66, name = 'bnd.code') 
+
+
+plotSPC(osds, width = 0.3, hz.distinctness.offset = 'hd', hz.topography.offset = 'hzto', cex.id = 0.66, cex.names = 0.66, name = 'bnd.code', hz.boundary.lty = 'hzto.lty') 
+
+legend('bottomleft', horiz = TRUE, legend = c('Smooth', 'Wavy', 'Irregular', 'Broken'), lty = 1:4, inset = 0.05, bty = 'n', cex = 0.85)
+
+
+plotSPC(osds, width = 0.3, hz.distinctness.offset = 'hd', hz.topography.offset = 'hzto', cex.id = 0.66, cex.names = 0.66, name = 'name', hz.boundary.lty = 'hzto.lty') 
+
+legend('bottomleft', horiz = TRUE, legend = c('Smooth', 'Wavy', 'Irregular', 'Broken'), lty = 1:4, inset = 0.05, bty = 'n', cex = 0.85)
+
 
 # keep a single OSD for the demo
 x <- filter(osds, id == 'LEON')
@@ -68,7 +85,7 @@ z <- combine(x, s)
 
 par(mar=c(0,0,1,1))
 plotSPC(z, width=0.33, print.id=TRUE, hz.distinctness.offset = 'hd', label='code', cex.names=0.8)
-title('Horizon Boundary Distinctness', line = 0)
+title('Horizon Boundary Distinctness', line = 0, col.main = 'white')
 
 # # not so very useful legend
 # par(xpd = NA)
@@ -107,20 +124,28 @@ z <- combine(x, s)
 
 par(mar=c(0,0,1,1))
 plotSPC(z, width=0.33, print.id=TRUE, hz.topography.offset = 'hzto', label='code', cex.names=0.8)
-title('Horizon Boundary Distinctness', line = 0)
+title('Horizon Boundary Distinctness', line = 0, col.main = 'white')
 
 
 
 
 ## combinations
 
-x <- filter(osds, id == 'ZOOK')
+x <- filter(osds, id == 'DRUMMER')
 
+# full set
 g <- expand.grid(
-  topography = c('S', 'W', 'I', 'B'),
-  distinctness = c('V', 'A', 'C', 'G', 'D')
-  
+  distinctness = c('V', 'A', 'C', 'G', 'D'),
+  topography = c('S', 'W', 'I', 'B')
 )
+
+# simplified set
+g <- expand.grid(
+  distinctness = c('A', 'C', 'G', 'D'),
+  topography = c('S', 'W', 'I')
+)
+
+
 
 l <- list()
 s <- duplicate(x, times = nrow(g))
@@ -130,19 +155,20 @@ for(i in 1:nrow(g)) {
   # set for all horizons
   horizons(ss)$hd <- hzDistinctnessCodeToOffset(g$distinctness[i])
   horizons(ss)$ht <- hzTopographyCodeToOffset(g$topography[i])
+  horizons(ss)$ht.lty <- hzTopographyCodeToLineType(g$topography[i]) 
   l[[i]] <- ss  
 }
 
 s <- combine(l)
-s$code <- sprintf("%s/%s", g$distinctness, g$topography)
+s$code <- sprintf("%s%s", g$distinctness, g$topography)
 
 par(mar=c(0,0,1,1), bg = 'black', fg = 'white')
-plotSPC(s, width=0.33, print.id=TRUE, hz.distinctness.offset = 'hd', hz.topography.offset = 'ht', label='code', cex.names=0.8, name = NA, color = NA, default.color = 'black')
+plotSPC(s, width=0.33, print.id=TRUE, hz.distinctness.offset = 'hd', hz.topography.offset = 'ht', label='code', cex.names=0.8, name = NA, color = NA, default.color = 'black', hz.boundary.lty = 'ht.lty')
 title('Horizon Boundary Types', line = 0, col.main = 'white')
-abline(h = x$top, col = 'green', lty = 3)
 
+p.seq <- rep(1:length(s), each = nrow(s[1, ]))
+points(x = p.seq, y = s$bottom, pch = 15, col = 'green', cex = 0.5)
 
-plot(0,0, type = 'n')
-
+legend('bottom', horiz = TRUE, legend = c('Smooth', 'Wavy', 'Irregular', 'Broken'), lty = 1:4, inset = 0.1)
 
 
