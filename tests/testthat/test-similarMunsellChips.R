62adeb997801853dd036dfb725367f2bba7a60bf
Andrew G. Brown <andrew.g.brown@usda.gov>
2021-01-05 21:46:11 -0800

    Slightly beefier test :cut_of_meat:

--- tests/testthat/test-similarMunsellChips.R
+++ tests/testthat/test-similarMunsellChips.R
@@ -1,4 +1,16 @@
 test_that("equivalentMunsellChips works", {
-  # the 5YR 1/1 and 7.5YR 1/1 are perceptually equivalent at the 0.1% quantile threshold
-  expect_equal(nrow(equivalentMunsellChips("7.5YR",1,1)[[1]]), 2)
+
+  # same "chip page position" different hue page; identify all perceptually equivalent chips
+
+  expect_equal(as.list(equivalentMunsellChips("7.5YR",2,1)[[1]][,c("hue","value","chroma")]), list(hue = c("10YR", "2.5YR", "5YR", "7.5YR"),
+                                                                                                   value = c(2,2,2,2),
+                                                                                                   chroma = c(1,1,1,1)))
+
+  expect_equal(as.list(equivalentMunsellChips("5YR",2,1)[[1]][,c("hue","value","chroma")]), list(hue = c("10R","2.5YR", "5YR", "7.5YR"),
+                                                                                                 value = c(2,2,2,2),
+                                                                                                 chroma = c(1,1,1,1)))
+
+  expect_equal(as.list(equivalentMunsellChips("10YR",2,1)[[1]][,c("hue","value","chroma")]), list(hue = c("10YR","2.5Y", "7.5YR"),
+                                                                                                  value = c(2,2,2),
+                                                                                                  chroma = c(1,1,1)))
 })
