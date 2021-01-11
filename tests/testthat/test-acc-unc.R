c763304cf20e2cfa17d30beb692eab1edd1b0dda
Dylan Beaudette <debeaudette@ucdavis.edu>
2021-01-11 11:06:38 -0800

    moving documentation over to roxygen style, more useful tests, minor bugfix in brierScore when input contained factors

--- tests/testthat/test-acc-unc.R
+++ tests/testthat/test-acc-unc.R
@@ -1,5 +1,32 @@
 context("classification accuracy and uncertainty")
 
+# a good classifier
+d.good <- data.frame(
+  a = c(0.05, 0.05, 0.10),
+  b = c(0.90, 0.85, 0.75),
+  c = c(0.05, 0.10, 0.15),
+  actual = c('b', 'b', 'b'),
+  stringsAsFactors = FALSE
+)
+
+# a rather bad classifier
+d.bad <- data.frame(
+  a = c(0.05, 0.05, 0.10),
+  b = c(0.90, 0.85, 0.75),
+  c = c(0.05, 0.10, 0.15),
+  actual = c('c', 'c', 'c'),
+  stringsAsFactors = FALSE
+)
+
+# class labels are factors
+d.factors <- data.frame(
+  a = c(0.05, 0.05, 0.10),
+  b = c(0.90, 0.85, 0.75),
+  c = c(0.05, 0.10, 0.15),
+  actual = c('b', 'b', 'b'),
+  stringsAsFactors = TRUE
+)
+
 # hypothetical three class probabilitites: 0.5 0.3 0.2
 probs <- c(5,3,2) / 10
 
@@ -19,4 +46,17 @@ test_that("brierScore", {
   set.seed(1)
   dff2 <- data.frame(matrix(runif(9), 3, 3))
   expect_equal(round(brierScore(dff2, c("X1","X2","X3")), 2), 1.31)
+  
+  # factor -> character conversion emits a message
+  expect_message(brierScore(x = d.factors, classLabels = c('a', 'b', 'c'), actual = 'actual'))
+  
+  # results should be the same
+  bs1 <- brierScore(x = d.good, classLabels = c('a', 'b', 'c'), actual = 'actual')
+  bs2 <- brierScore(x = d.factors, classLabels = c('a', 'b', 'c'), actual = 'actual')
+  
+  expect_equal(bs1, bs2)
 })
+
+
+
+
