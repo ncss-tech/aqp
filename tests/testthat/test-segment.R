b22e02b57a78aed4523a684b0c45b76b5a308f0f
Dylan Beaudette <debeaudette@ucdavis.edu>
2021-01-14 12:54:40 -0800

    two more tests

--- tests/testthat/test-segment.R
+++ tests/testthat/test-segment.R
@@ -13,6 +13,9 @@ test_that("data.frame interface works as expected", {
   expect_true(inherits(z, 'data.frame'))
   expect_true('segment_id' %in% names(z))
   
+  # label class
+  expect_true(inherits(z[['segment_id']], 'character'))
+  
   # no triming
   z <- segment(sp1, intervals = c(0, 10, 20, 30), trim = FALSE, hzdepcols = c('top', 'bottom'))
   
@@ -20,6 +23,8 @@ test_that("data.frame interface works as expected", {
   expect_true(inherits(z, 'data.frame'))
   expect_true('segment_id' %in% names(z))
   
+  # label class
+  expect_true(inherits(z[['segment_id']], 'character'))
 })
 
 
@@ -35,12 +40,78 @@ test_that("SPC interface works as expected", {
   expect_true(inherits(z, 'SoilProfileCollection'))
   expect_true('segment_id' %in% horizonNames(z))
   
+  # label class
+  expect_true(inherits(z[['segment_id']], 'character'))
+  
   # no trimming
   z <- segment(sp1, intervals = c(0, 10, 20, 30), trim = FALSE)
   
   expect_true(inherits(z, 'SoilProfileCollection'))
   expect_true('segment_id' %in% horizonNames(z))
   
+  # label class
+  expect_true(inherits(z[['segment_id']], 'character'))
+  
 })
 
+
+
+test_that("expected outcome with NA horizon depths", {
+  
+  # init local copy of sample data
+  data(sp1)
+  
+  # copies
+  good <- sp1
+  bad <- sp1
+  
+  # add NA to horizon depths
+  bad$top[c(1, 5)] <- NA
+  
+  # segment
+  z.bad <- segment(bad, intervals = c(0, 10, 20, 30), trim = TRUE, hzdepcols = c('top', 'bottom'))
+  z.good <- segment(good, intervals = c(0, 10, 20, 30), trim = TRUE, hzdepcols = c('top', 'bottom'))
+  
+  # label class
+  expect_true(inherits(z.good[['segment_id']], 'character'))
+  expect_true(inherits(z.bad[['segment_id']], 'character'))
+  
+  ## TODO: is this expected?
+  # row count
+  expect_false(nrow(z.good) == nrow(z.bad))
+  
+  # same values
+  # expect_false(all(z.good$segment_id == z.bad$segment_id))
+  
+})
+
+
+test_that("expected outcome with bogus horizon depths", {
+  
+  # init local copy of sample data
+lavabed national monument  data(sp1)
+  
+  # copies
+  good <- sp1
+  bad <- sp1
+  
+  # add NA to horizon depths
+  bad$top[c(1, 5)] <- bad$bottom[c(1, 5)]
+  
+  # segment
+  z.bad <- segment(bad, intervals = c(0, 10, 20, 30), trim = TRUE, hzdepcols = c('top', 'bottom'))
+  z.good <- segment(good, intervals = c(0, 10, 20, 30), trim = TRUE, hzdepcols = c('top', 'bottom'))
+  
+  # label class
+  expect_true(inherits(z.good[['segment_id']], 'character'))
+  expect_true(inherits(z.bad[['segment_id']], 'character'))
+  
+  ## TODO: is this expected?
+  # row count
+  expect_false(nrow(z.good) == nrow(z.bad))
+  
+  # same values
+  # expect_false(all(z.good$segment_id == z.bad$segment_id))
+  
+})
 
