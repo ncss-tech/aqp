526e503407d5bfb901db89c322f1e95b3f36282e
Andrew G. Brown <andrew.g.brown@usda.gov>
2021-01-16 23:36:23 -0800

    mixMunsell: update tests for custom weights

--- tests/testthat/test-mixMunsell.R
+++ tests/testthat/test-mixMunsell.R
@@ -3,7 +3,6 @@ context("mixing Munsell colors")
 
 test_that("mixMunsell works as expected", {
 
-
   ## error conditions
   expect_error(mixMunsell(c(NA, '10YR 3/4')))
 
@@ -21,21 +20,23 @@ test_that("mixMunsell works as expected", {
   x <- mixMunsell(c('10YR 5/3', '10YR 3/2'))
   expect_true(x$munsell == '10YR 4/2')
 
-  # TODO: tests to "fix"
+  # weights when length(x) != length(unique(x))
+  expect_silent(mixMunsell(c('10YR 5/3', '10YR 3/2', '10YR 5/3')))
 
-  # mixMunsell-patch #183 to fix this (bad weights when length(x) != length(unique(x)))
-  expect_warnings(mixMunsell(c('10YR 5/3', '10YR 3/2', '10YR 5/3')))
+  # 0 weights to filter NA
+  expect_silent(mixMunsell(c(NA, '10YR 3/4'), w = c(0, 1)))
 
-  # also: propose allowing 0 weights to filter NA
-
-  # Error in mixMunsell(c(NA, "10YR 3/4"), w = c(0, 1)) :
-  # input must be valid Munsell notation, neutral hues not supported
-  expect_error(mixMunsell(c(NA, '10YR 3/4'), w = c(0, 1)))
-
-  # also: pretty sure this output does not make sense
+  # also: pretty sure this output did not make sense
   # mixMunsell(c('10YR 5/3', '10YR 3/2'), w = 1)
   # munsell   distance
   # 1 7.5GY 2/4 0.07119279
+
+  x <- mixMunsell(c('10YR 5/3', '10YR 3/2'), w = 1)
+  expect_true(x$munsell == '10YR 4/2')
+
+  # correct output, as above; using recycled weight length 1
+  # munsell   distance
+  # 1 10YR 4/2 0.02993819
 })
 
 
