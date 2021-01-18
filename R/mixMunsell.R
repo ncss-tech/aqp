87bb151cc8cd53ca5e0b18856e3ee71cf28a638b
Andrew Gene Brown <andrew.g.brown@usda.gov>
2021-01-18 08:30:37 -0800

    mixMunsell patch (#183) - more tests
    * mixMunsell: add handling for duplication, 0 weights, missing data
    
    * mixMunsell: tests
    
    * mixMunsell: update tests for custom weights
    
    * mixMunsell: 1 unique input; 1 row output

--- R/mixMunsell.R
+++ R/mixMunsell.R
@@ -3,36 +3,36 @@
 
 # helper function for printing out value / chroma ranges by hue
 .summarizeMunsellSpectraRanges <- function() {
-  
+
   # make R CMD CHECK happy
   munsell.spectra <- NULL
-  
+
   # note: this is incompatible with LazyData: true
   # load look-up table from our package
   load(system.file("data/munsell.spectra.rda", package="aqp")[1])
-  
+
   # set hue position
   munsell.spectra$hue <- factor(munsell.spectra$hue, levels = huePosition(returnHues = TRUE))
-  
+
   # remove non-standard hues (what are they for?)
   munsell.spectra <- na.omit(munsell.spectra)
-  
+
   x <- split(munsell.spectra, munsell.spectra$hue)
-  
+
   x <- lapply(x, function(i) {
-    
+
     data.frame(
       hue = i$hue[1],
       value = sprintf("%s-%s", min(i$value), max(i$value)),
       chroma = sprintf("%s-%s", min(i$chroma), max(i$chroma)),
       stringsAsFactors = FALSE
     )
-    
+
   })
-  
-  
+
+
   x <- do.call('rbind', x)
-  
+
   return(x)
 }
 
@@ -78,30 +78,30 @@
 # * https://github.com/HenrikBengtsson/matrixStats
 
 
-#' 
+#'
 #' @title Mix Munsell Colors via Spectral Library
-#' 
+#'
 #' @description Simulate subtractive mixing of colors in Munsell notation, similar to the way in which mixtures of pigments operate.
-#' 
+#'
 #' @param x vector of colors in Munsell notation
-#' 
+#'
 #' @param w vector of proportions, can sum to any number
-#' 
+#'
 #' @param n number of closest matching color chips
-#' 
+#'
 #' @author D.E. Beaudette
-#' 
-#' @references 
-#' 
+#'
+#' @references
+#'
 #' \itemize{
 #'    \item{inspiration / calculations based on the work of Scott Burns: }{\url{https://arxiv.org/ftp/arxiv/papers/1710/1710.06364.pdf}}
-#'    
+#'
 #'    \item{related discussion on Stack Overflow: }{\url{https://stackoverflow.com/questions/10254022/implementing-kubelka-munk-like-krita-to-mix-colours-color-like-paint/29967630#29967630}}
-#'    
+#'
 #'    \item{spectral library source: }{\url{http://www.munsellcolourscienceforpainters.com/MunsellResources/SpectralReflectancesOf2007MunsellBookOfColorGlossy.txt}}
-#' 
+#'
 #' }
-#' 
+#'
 #'
 #' @details
 #' Pending
@@ -109,35 +109,66 @@
 #' @note This functions is slower than \code{soilDB::estimateColorMixture()} (weighted average of colors in CIELAB coordinates) but more accurate.
 #'
 #' @return a \code{data.frame} with the closest matching Munsell color(s)
-#'  
+#'
 #' @seealso \code{\link{munsell.spectra}}
-#'  
+#'
 mixMunsell <- function(x, w = rep(1, times = length(x)) / length(x), n = 1) {
-  
+
   ## TODO
   # sanity checks
-  
-    
-  # x and w same lengths
-  
-  # x contains valid Munsell
-  if(any(is.na(parseMunsell(x)))) {
-    stop('input must be valid Munsell notation, neutral hues not supported')
+
+  # can't mix a single color, just give it back at 0 distance
+  if (length(unique(x)) == 1) {
+    return(data.frame(munsell = x[1], distance = 0))
   }
-  
+
+  # must have as many weights as length of x
+  if (length(x) != length(w) && length(w) != 1) {
+
+    stop('w should have same length as x or length one')
+
+  } else if (length(w) == 1) {
+
+    # cannot mix with zero weights
+    stopifnot(w > 0)
+
+    # a recycled weight is same as function default
+    w <- rep(1, times = length(x)) / length(x)
+
+  }
+
+  # more informative error for colors missing
+  if (any(w[is.na(x)] > 0)) {
+    stop('cannot mix missing (NA) colors with weight greater than zero')
+  }
+
+  # more informative error for weights missing
+  if (any(is.na(w))) {
+    stop('cannot mix colors with missing (NA) weight')
+  }
+
+  # remove 0-weighted colors
+  x <- x[w > 0]
+  w <- w[w > 0]
+
+  # x with weights > 0 must contain valid Munsell
+  if (any(is.na(parseMunsell(x)))) {
+    stop('input must be valid Munsell notation, neutral hues and missing not supported')
+  }
+
   # satisfy R CMD check
   munsell.spectra.wide <- NULL
-  
+
   # wide version for fast searches
   load(system.file("data/munsell.spectra.wide.rda", package="aqp")[1])
-  
+
   # subset reference spectra for colors
   # note that search results are not in the same order as x
   # result are columns of spectra
   munsell.names <- names(munsell.spectra.wide)
   idx <- which(munsell.names %in% x)
   s <- munsell.spectra.wide[, idx, drop = FALSE]
-  
+
   # sanity check: if there aren't sufficient reference spectra then return NA
   # must be at least the same number of spectra (columns) as unique colors specified
   if(ncol(s) < length(unique(x))){
@@ -149,52 +180,54 @@ mixMunsell <- function(x, w = rep(1, times = length(x)) / length(x), n = 1) {
     )
     return(res)
   }
-  
+
   # empty vector for mixture
   mixed <- vector(mode = 'numeric', length = nrow(s))
-  
+
   # iterate over wavelength (columns in first spectra)
   for(i in seq_along(mixed)) {
-    
+
     # prepare values:
     # select the i-th wavelength (row)
     # down-grade to a vector
     vals <- unlist(s[i, ])
-    
+
+    # aggregate weights by "chip" -- in case length(x) != length(unique(x))
+    wagg <- aggregate(w, by = list(chip = x), FUN = sum)
+
     # mix via weighted geometric mean
-    mixed[i] <- .wgm( v = vals, w = w )
+    mixed[i] <- .wgm(v = vals, w = wagg$x[match(names(s), wagg$chip)])
   }
-  
-  
+
   ## TODO: what is the "best" distance metric when comparing 1 spectra to the entire library?
-  
+
   ## optimization: matrixStats::colSums2() much faster
   ## --> syntax slightly different
-  
+
   ## for now, using Euclidean distance
   # D = sqrt(sum( [reference - mixed]^2 ))
-  
+
   # subtract the mixture spectra, element-wise, from reference library
   # note we are removing the wavelength (1st) column
   m.diff <- sweep(munsell.spectra.wide[, -1], MARGIN = 1, STATS = mixed, FUN = '-')
-  
+
   # finish the distance calculation
   m.dist <- sqrt(colSums(m.diff^2))
-  
+
   # get the spectra of the closest n munsell chip(s) via sorting ASC
   m.match <- sort(m.dist)[1:n]
-  
+
   # compile into data.frame
   res <- data.frame(
     munsell = names(m.match),
     distance = m.match,
     stringsAsFactors = FALSE
   )
-  
+
   # clean-up row names
-  row.names(res) <- as.character(1:nrow(res))
-  
+  row.names(res) <- NULL
+
   return(res)
-  
+
 }
 
--- tests/testthat/test-mixMunsell.R
+++ tests/testthat/test-mixMunsell.R
@@ -3,7 +3,6 @@ context("mixing Munsell colors")
 
 test_that("mixMunsell works as expected", {
 
-
   ## error conditions
   expect_error(mixMunsell(c(NA, '10YR 3/4')))
 
@@ -20,22 +19,20 @@ test_that("mixMunsell works as expected", {
   # standard usage
   x <- mixMunsell(c('10YR 5/3', '10YR 3/2'))
   expect_true(x$munsell == '10YR 4/2')
-
-  # TODO: tests to "fix"
-
-  # mixMunsell-patch #183 to fix this (bad weights when length(x) != length(unique(x)))
-  expect_warning(mixMunsell(c('10YR 5/3', '10YR 3/2', '10YR 5/3')))
-
-  # also: propose allowing 0 weights to filter NA
-
-  # Error in mixMunsell(c(NA, "10YR 3/4"), w = c(0, 1)) :
-  # input must be valid Munsell notation, neutral hues not supported
-  expect_error(mixMunsell(c(NA, '10YR 3/4'), w = c(0, 1)))
-
-  # also: pretty sure this output does not make sense
-  # mixMunsell(c('10YR 5/3', '10YR 3/2'), w = 1)
+  
+  # correct output, same as above; using recycled weight length 1
   # munsell   distance
-  # 1 7.5GY 2/4 0.07119279
+  # 1 10YR 4/2 0.02993819
+  
+  x <- mixMunsell(c('10YR 5/3', '10YR 3/2'), w = 1)
+  expect_true(x$munsell == '10YR 4/2')
+
+  # weights when length(x) != length(unique(x))
+  expect_silent(mixMunsell(c('10YR 5/3', '10YR 3/2', '10YR 5/3')))
+
+  # 0 weights to filter NA
+  expect_silent(mixMunsell(c(NA, '10YR 3/4'), w = c(0, 1)))
+
 })
 
 
