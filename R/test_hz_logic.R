9b690bb2af2f8259307d95272aefb1e95126bbed
Andrew G. Brown <andrew.g.brown@ca.usda.gov>
2021-01-20 16:40:19 -0800

    Revert "#190; looks like this got uncommented when I rebased onto latest changes"
    This reverts commit f63d623dfc0791ea033b2706094d28362b42f584.

--- R/test_hz_logic.R
+++ R/test_hz_logic.R
@@ -1,44 +1,44 @@
-# ## https://github.com/ncss-tech/aqp/issues/65
-# 
-# # deprecated
-# test_hz_logic <- function(i, topcol, bottomcol, strict=FALSE) {
-#   
-#   ## not using this function any longer, will remove by aqp 2.0
-#   .Deprecated('checkHzDepthLogic')
-#   
-#   
-#   # test for NA
-#   if(any(c(is.na(i[[topcol]])), is.na(i[[bottomcol]]))) {
-#     res <- FALSE
-#     names(res) <- 'hz_logic_pass'
-#     return(res)
-#   }
-#   
-# 	# test for overlapping OR non-contiguous horizon boundaries
-# 	if(strict) {
-# 		n <- nrow(i)
-# 		res <- all.equal(i[[topcol]][-1], i[[bottomcol]][-n])
-# 		if(res != TRUE)
-# 			res <- FALSE
-# 		names(res) <- 'hz_logic_pass'
-# 		return(res)
-# 	}
-# 	
-# 
-# 	  
-# 	# test for overlapping horizons
-# 	# note: this will fail if an O horizon is described using the old style O 3--0cm
-# 	m <- cbind(i[[topcol]], i[[bottomcol]])
-# 	unzipped.depths <- unlist(apply(m, 1, function(i) seq(from=i[1], to=i[2], by=1)))
-# 	len.overlapping <- length(which(table(unzipped.depths) > 1))
-# 	n.hz <- nrow(i)
-# 	
-# 	# there should be 1 fewer segments of overlap than there are horizons  	
-# 	if(len.overlapping > (n.hz - 1))
-# 		res <- FALSE
-# 	else
-# 		res <- TRUE
-# 	names(res) <- 'hz_logic_pass'
-# 	return(res)
-#   
-# }
+## https://github.com/ncss-tech/aqp/issues/65
+
+# depricated
+test_hz_logic <- function(i, topcol, bottomcol, strict=FALSE) {
+  
+  ## not using this function any longer, will remove by aqp 2.0
+  .Deprecated('checkHzDepthLogic')
+  
+  
+  # test for NA
+  if(any(c(is.na(i[[topcol]])), is.na(i[[bottomcol]]))) {
+    res <- FALSE
+    names(res) <- 'hz_logic_pass'
+    return(res)
+  }
+  
+	# test for overlapping OR non-contiguous horizon boundaries
+	if(strict) {
+		n <- nrow(i)
+		res <- all.equal(i[[topcol]][-1], i[[bottomcol]][-n])
+		if(res != TRUE)
+			res <- FALSE
+		names(res) <- 'hz_logic_pass'
+		return(res)
+	}
+	
+
+	  
+	# test for overlapping horizons
+	# note: this will fail if an O horizon is described using the old style O 3--0cm
+	m <- cbind(i[[topcol]], i[[bottomcol]])
+	unzipped.depths <- unlist(apply(m, 1, function(i) seq(from=i[1], to=i[2], by=1)))
+	len.overlapping <- length(which(table(unzipped.depths) > 1))
+	n.hz <- nrow(i)
+	
+	# there should be 1 fewer segments of overlap than there are horizons  	
+	if(len.overlapping > (n.hz - 1))
+		res <- FALSE
+	else
+		res <- TRUE
+	names(res) <- 'hz_logic_pass'
+	return(res)
+  
+}
