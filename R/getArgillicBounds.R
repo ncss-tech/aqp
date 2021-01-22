e5ab303ea10ee8908342db7e7c83c8d7909d9064
Andrew G. Brown <andrew.g.brown@ca.usda.gov>
2021-01-22 08:15:33 -0800

    argillic.clay.increase.depth -> getArgillicBounds.R

--- R/getArgillicBounds.R
+++ R/getArgillicBounds.R
@@ -293,14 +293,35 @@ crit.clay.argillic <- function(eluvial_clay_content) {
   return(round(buf))
 }
 
-#' Argillic Clay Increase Depth
-#'
-#' @param p A single profile SoilProfileCollection
-#' @param clay.attr The horizon column name containing clay data; default: `'clay'`
-#'
-#' @return A numeric vector of the increase depth, given `crit.clay.argillic` as the threshold and a vertical distance threshold of `30` cm.
-#' @export
-#'
+#' Return upper boundary of argillic horizon
+#' 
+#' Returns the top depth of the argillic horizon as a numeric vector.
+#' 
+#' Uses \code{crit.clay.argillic} to determine threshold clay increase, and
+#' \code{get.increase.matrix} to determine where increase is met within a
+#' vertical distance of 30 cm.
+#' 
+#' 
+#' @param p A single-profile \code{SoilProfileCollection} object.
+#' @param clay.attr OPTIONAL: horizon attribute name referring to clay content.
+#' default: `clay`
+#' @return A numeric vector containing top depth of argillic horizon, if
+#' present, or NA.
+#' @author Andrew Gene Brown
+#' @seealso \code{getArgillicBounds}, \code{get.increase.matrix},
+#' \code{crit.clay.argillic}
+#' @keywords manip
+#' @examples
+#' 
+#' data(sp1, package = 'aqp')
+#' depths(sp1) <- id ~ top + bottom
+#' site(sp1) <- ~ group
+#' 
+#' p <- sp1[1]
+#' attr <- 'prop' # clay contents 
+#' foo <- argillic.clay.increase.depth(p, clay.attr = attr)
+#' foo
+#' 
 argillic.clay.increase.depth <- function(p, clay.attr = 'clay') {
   vd <- 30
   return(get.increase.depths(p, attr = clay.attr,
