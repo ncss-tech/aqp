16f8020259e17a5da626d118aeaf59a9346c0fec
Andrew G. Brown <andrew.g.brown@usda.gov>
2021-02-13 12:32:04 -0800

    docs

--- R/mutate.R
+++ R/mutate.R
@@ -1,8 +1,9 @@
 #' @title Transform a SPC with expressions based on site or horizon level attributes
 #' @name mutate
-#' @aliases mutate,SoilProfileCollection-method
+#' @aliases mutate,SoilProfileCollection-method, transform,SoilProfileCollection-method
 #' @description \code{mutate()} is a function used for transforming SoilProfileCollections. It allows the user to specify an arbitrary number of expressions that resolve to the (re-)calculation of one or more site or horizon level attributes. For instance: \code{mutate(spc, thickness = hzdepb - hzdept)}. The expressions may depend on one another, and are evaluated from left to right.
 #' @param object A SoilProfileCollection
+#' @param `_data` A SoilProfileCollection
 #' @param ... Comma-separated set of R expressions e.g. \code{thickness = hzdepb - hzdept, hzdepm = hzdept + round(thk / 2)}
 #' @return A SoilProfileCollection.
 #' @author Andrew G. Brown.
@@ -13,23 +14,29 @@ if (!isGeneric("mutate"))
   setGeneric("mutate", function(object, ...) standardGeneric("mutate"))
 
 setMethod("mutate", signature(object = "SoilProfileCollection"), function(object, ...) {
+  # .Deprecated("transform")
+  aqp::transform(object, ...)
+})
 
-    # capture expression(s) at function
-    .dots <- substitute(list(...))
-    .dots <- .dots[2:length(.dots)]
-    .names <- names(.dots)
+#' @export
+setMethod("transform", signature(`_data` = "SoilProfileCollection"), function(`_data`, ...) {
 
-    if (is.null(.names))
-      .names <- as.character(.dots)
+  # create composite object to facilitate eval
+  .data <- compositeSPC(`_data`)
 
-    # create composite object to facilitate eval_tidy
-    .data <- compositeSPC(object)
+  # capture expression(s) at function
+  .dots <- substitute(list(...))
+  .dots <- .dots[2:length(.dots)]
+  .names <- names(.dots)
 
-    for(n in 1:length(.dots)) {
-      foo <- .data_dots(.data, eval(.dots[[n]]))[[1]]
-      object[[.names[n]]] <- foo
-      .data[[.names[n]]] <- foo
-    }
+  if (is.null(.names))
+    .names <- as.character(.dots)
 
-    return(object)
+  for(n in 1:length(.dots)) {
+    foo <- .data_dots(.data, eval(.dots[[n]]))[[1]]
+    `_data`[[.names[n]]] <- foo
+    .data[[.names[n]]] <- foo
+  }
+
+  return(`_data`)
 })
