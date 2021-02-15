3d890da9e1696746fb48b06786e9fe16857a8b6b
Andrew G. Brown <andrew.g.brown@usda.gov>
2021-02-15 11:35:00 -0800

    Best-available data.table solution

--- R/getLastHorizonID.R
+++ R/getLastHorizonID.R
@@ -1,29 +1,34 @@
-# 
+#
 #' @title Get IDs of Deepest Horizons by Profile
-#' 
+#'
 #' @description Return horizon IDs of the deepest horizon within each profile of a `SoilProfileCollection`. IDs are returned in the same order as `profile_id(x)`. Horizon top depths are used because there are cases where bottom depths may be missing.
-#' 
+#'
 #' @param x a `SoilProfileCollection`
-#' 
+#'
 getLastHorizonID <- function(x) {
-  
-  # satisfy R CMD check
-  .SD <- NULL
-  .N <- NULL
-  
+
+  # with data.table
   hztb <- horizonDepths(x)
   hzidn <- hzidname(x)
   idn <- idname(x)
-  
-  # iterate over profile horizon subset data.frame; return deepest horizon IDs
-  # data.table upgrade c/o AGB
+
+  .N <- NULL
+  .I <- NULL
+
+  # ensure site and horizons are "in sync"
+  stopifnot(spc_in_sync(x)$valid)
+
   h <- data.table::as.data.table(horizons(x))
-  
-  h.sub <- h[, .SD[.N,], by = list(h[[idn]])]
-  res <- h.sub[[hzidn]]
-  
-  names(res) <- h.sub[[idn]]
-  
+
+  # get row index of last horizon by group
+  idx <- h[, .I[.N], by = list(h[[idn]])]$V1
+
+  # get hzID for just last horizon
+  res <- hzID(x)[idx]
+
+  # add profile IDs as names
+  names(res) <- profile_id(x)
+
   # a named vector, in the same order as profile_id(x)
   return(res)
 }
