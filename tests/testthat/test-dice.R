a9f91f4e0632670d3c56ffeb9c31e19559571d8b
Dylan Beaudette <debeaudette@ucdavis.edu>
2021-02-24 13:45:55 -0800

    more tests, #115

--- tests/testthat/test-dice.R
+++ tests/testthat/test-dice.R
@@ -1,12 +1,13 @@
 context("dice (update to slice)")
 
+data(sp4, package = 'aqp')
+depths(sp4) <- id ~ top + bottom
+
 test_that("basic functionality", {
-  
-  data(sp4, package = 'aqp')
-  depths(sp4) <- id ~ top + bottom
   
   # SPC
   s <- dice(sp4)
+  
   # as a data.frame
   s.d <- dice(sp4, SPC = FALSE)
   
@@ -15,13 +16,85 @@ test_that("basic functionality", {
   expect_true(inherits(s.d, 'data.frame'))
   
   # new metadata columns
+  expect_true(all(c('sliceID', '.oldTop', '.oldBottom') %in% horizonNames(s)))
+  expect_true(all(c('sliceID', '.oldTop', '.oldBottom') %in% names(s.d)))
   
-  # arguments
-  
-  # there should be 42 horizon slices
-  expect_equal(nrow(horizons(s[1, ])),  42)
+  # there should be as many slices as original profiles are deep
+  # names should be identical
+  expect_equal(
+    profileApply(sp4, max),
+    profileApply(s, nrow)
+  )
   
 })
 
+test_that("formula interface", {
+  
+  # reference
+  s <- dice(sp4)
+  
+  # equivalent to `fm = NULL`
+  s1 <- dice(sp4, fm = ~ .)
+  expect_equivalent(s, s1)
+  
+  # LHS
+  s2 <- dice(sp4, fm = 0:20 ~ .)
+  
+  # all should be <= 21 slices
+  expect_true(all(profileApply(s2, nrow) <= 21))
+  
+  # RHS
+  s3 <- dice(sp4, fm = ~ Ca)
+  
+  # all other hz attr should be missing
+  hz.diff <- setdiff(horizonNames(sp4), horizonNames(s3))
+  expect_false(
+    all(
+      horizonNames(s3) %in% c("name", "K", "Mg", "CEC_7", "ex_Ca_to_Mg", "sand", "silt", "clay", "CF")
+    )
+  )
+
+  # LHS + RHS
+  s4 <- dice(sp4, fm = 0:30 ~ Ca + K)
+  
+  # hz names
+  expect_false(
+    all(
+      horizonNames(s4) %in% c("name", "Mg", "CEC_7", "ex_Ca_to_Mg", "sand", "silt", "clay", "CF")
+    )
+  )
+  
+  expect_true(all(profileApply(s4, nrow) <= 31))
+  
+})
+
+
+test_that("percent missing calculation", {
+  
+  sp4$K[c(1, 5, 10)] <- NA
+  sp4$Ca[c(1, 5, 20)] <- NA
+  
+  expect_warning(s <- dice(sp4, fm = ~ Ca + K, pctMissing = TRUE))
+  
+  # visual check
+  # plotSPC(s, color = '.pctMissing')
+  
+  # new column
+  expect_true('.pctMissing' %in% horizonNames(s))
+  
+  # some should be non-zero
+  expect_true(any(s$.pctMissing > 0) & ! all(s$.pctMissing > 0))
+})
+
+
+test_that("padding with NA, backwards-compat with slice", {
+  
+  
+  s <- dice(sp4, fm = 0:80 ~ ., padNA = TRUE)
+  
+  # all profiles should be the same "depth", including empty (NA) horizons
+  expect_true(all(profileApply(s, max) == 80))
+  
+})
 
 
