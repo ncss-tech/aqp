46a4aaab1f60896f7a67335620bc67c72a585c2b
Dylan Beaudette <debeaudette@ucdavis.edu>
2021-04-02 22:34:58 -0700

    adding an empirical scaling factor for interpreting spectral distance

--- R/mixMunsell.R
+++ R/mixMunsell.R
@@ -20,9 +20,11 @@
   m <- rgb2munsell(mixed.color[, c('r', 'g', 'b')])
   
   # pack into expected structure
+  # scaled distance is only for spectral distance evaluated against the entire library
   res <- data.frame(
     munsell = sprintf('%s %s/%s', m$hue, m$value, m$chroma),
     distance = m$sigma,
+    scaledDistance = NA,
     mixingMethod = 'estimate',
     stringsAsFactors = FALSE
   )
@@ -80,13 +82,13 @@
 }
 
 
-# another other possible approach using only sRGB coordinates
+# another possible approach using only sRGB coordinates
 # http://scottburns.us/wp-content/uploads/2015/04/ILSS.txt
 
 # related ticket
 # https://github.com/ncss-tech/aqp/issues/101
 
-# in response to the dumb commentary here:
+# in response to the commentary here:
 # https://soiltxnmyforum.cals.vt.edu/forum/read.php?3,1984,1987#msg-1987
 
 # inspiration / calculations based on:
@@ -107,7 +109,6 @@
 # * 80% of time is spent on sweep() and colSums()
 # * see matrixStats package for a compiled version: colSums2()
 # * https://github.com/HenrikBengtsson/matrixStats
-# * use gower package, compiled code, new dependency -> 20x faster
 
 
 #'
@@ -127,6 +128,8 @@
 #' @param n number of closest matching color chips (`mixingMethod = spectra` only)
 #'
 #' @param keepMixedSpec keep weighted geometric mean spectra, final result is a `list` (`mixingMethod = spectra` only)
+#' 
+#' @param distThreshold spectral distance used to compute `scaledDistance`, default value is based on an analysis of spectral distances associated with adjacent Munsell color chips.
 #'
 #' @author D.E. Beaudette
 #'
@@ -143,13 +146,42 @@
 #'
 #'
 #' @details
-#' Pending
+#' An accurate simulation of pigment mixtures ("subtractive" color mixtures) is incredibly complex due to factors that aren't easily measured or controlled: pigment solubility, pigment particle size distribution, water content, substrate composition, and physical obstruction to name a few. That said, it is possible to simulate reasonable, subtractive color mixtures given a reference spectra library (350-800nm) and some assumptions about pigment qualities and lighting. For the purposes of estimating a mixture of soil colors (these are pigments after all) we can relax these assumptions and assume a standard light source. The only missing piece is the spectral library for all Munsell chips in our color books.
+#' 
+#' Thankfully, [Scott Burns has outlined the entire process](https://arxiv.org/ftp/arxiv/papers/1710/1710.06364.pdf), and Paul Centore has provided a Munsell color chip [reflectance spectra library](http://www.munsellcolourscienceforpainters.com/). The estimation of a subtractive mixture of soil colors can proceed as follows:
+#'   
+#' 1. look up the associated spectra for each color
+#' 2. computed the weighted (area proportion) geometric mean of the spectra
+#' 3. search for the closest matching spectra in the reference library
+#' 4. suggest that Munsell chip as the best candidate for the simulated mixture
+#' 
+#' Key assumptions include:
+#'   
+#' * similar particle size distribution
+#' * similar mineralogy (i.e. pigmentation qualities)
+#' * similar water content. 
+#' 
+#' For the purposes of estimating (for example) a "mixed soil color within the top 18cm of soil" these assumptions are usually valid. Again, these are estimates that are ultimately "snapped" to the nearest chip and not do not need to approach the accuracy of paint-matching systems.
+#' 
+#' A message is printed when `scaledDistance` is larger than 1.
 #'
-#' @return A `data.frame` with the closest matching Munsell color(s), unless `keepMixedSpec = TRUE` then a `list`.
+#' @return A `data.frame` with the closest matching Munsell color(s):
+#' 
+#'  * `munsell`: Munsell notation of the n-closest spectra
+#'  * `distance`: spectral (Gower) distance to the n-closest spectra
+#'  * `scaledDistance`: spectral distance scaled by `distThreshold`
+#'  * `mixingMethod`: method used for each mixture
+#' 
+#' When `keepMixedSpec = TRUE` then a `list`:
+#' 
+#'  * `mixed`: a `data.drame` containing the same elements as above
+#'  * `spec`: spectra for the 1st closest match
+#' 
+#' 
 #'
 #' @seealso \code{\link{munsell.spectra}}
 #'
-mixMunsell <- function(x, w = rep(1, times = length(x)) / length(x), mixingMethod = c('spectra', 'estimate', 'adaptive'), n = 1, keepMixedSpec = FALSE) {
+mixMunsell <- function(x, w = rep(1, times = length(x)) / length(x), mixingMethod = c('spectra', 'estimate', 'adaptive'), n = 1, keepMixedSpec = FALSE, distThreshold = 0.025) {
 
   # satisfy R CMD check
   munsell.spectra.wide <- NULL
@@ -251,6 +283,7 @@ mixMunsell <- function(x, w = rep(1, times = length(x)) / length(x), mixingMetho
         res <- data.frame(
           munsell = NA,
           distance = NA,
+          scaledDistance = NA,
           mixingMethod = NA,
           stringsAsFactors = FALSE
         )
@@ -304,6 +337,7 @@ mixMunsell <- function(x, w = rep(1, times = length(x)) / length(x), mixingMetho
     res <- data.frame(
       munsell = dimnames(z)[[1]][d$index],
       distance = d$distance[, 1],
+      scaledDistance = d$distance[, 1] / distThreshold,
       mixingMethod = 'spectra',
       stringsAsFactors = FALSE
     )
@@ -328,6 +362,11 @@ mixMunsell <- function(x, w = rep(1, times = length(x)) / length(x), mixingMetho
     #   distance = m.match,
     #   stringsAsFactors = FALSE
     # )
+    
+    # report possibly problematic mixtures
+    if(any(res$scaledDistance > 1)) {
+      message('closest match has a spectral distance that is large, results may be unreliable')
+    }
     
     # clean-up row names
     row.names(res) <- NULL
