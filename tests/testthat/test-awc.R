5610ce7cb7b57adea1b44cd02ceb9a697178b915
Andrew G. Brown <andrew.g.brown@usda.gov>
2021-05-23 13:44:50 -0700

    Use round() in expectation

--- tests/testthat/test-awc.R
+++ tests/testthat/test-awc.R
@@ -4,16 +4,16 @@ test_that('basic functionality', {
   
   # test range of organic matter, loam FEF textures
   base.awc <- estimateAWC(c("l","l","l"), c(3, 2, 1), na.rm = TRUE)
-  expect_equal(base.awc, c(0.21, 0.18, 0.17))
+  expect_equal(base.awc, c(0.21, 0.18, round(0.175, 2)))
   
   # auto-recycling of singletons, 3 AWCs all corrected with 25% rf
   corrected.awc <- correctAWC(base.awc, total_rf = 25)
-  expect_equal(corrected.awc, c(0.18, 0.15, 0.14))
+  expect_equal(corrected.awc, c(0.18, 0.15, round(0.145, 2)))
   
   # using gravel-size fragments often gives somewhat more restrictive AWC 
   corrected.awc2 <- correctAWC(base.awc, total_rf = 25, gravel = 25)
-  expect_equal(corrected.awc2, c(0.16, 0.15, 0.14))
+  expect_equal(corrected.awc2, c(0.16, 0.15, round(0.145, 2)))
   
   salty.awc <- correctAWC(base.awc, total_rf = 0, ec = 8)
-  expect_equal(salty.awc, c(0.17, 0.14, 0.14))
+  expect_equal(salty.awc, c(0.17, 0.14, round(0.145, 2)))
 })
