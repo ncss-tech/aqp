250cf7bd881aa031a305c38f7cfea165a50dae51
Dylan Beaudette <debeaudette@ucdavis.edu>
2021-07-14 14:48:40 -0700

    that is better

--- misc/sandbox/parseMunsell-profiling.R
+++ misc/sandbox/parseMunsell-profiling.R
@@ -14,9 +14,11 @@ x4 <- rep('10YR 4/4', times = 1e6)
 x5 <- rep('10YR 4/4', times = 1e7)
 
 microbenchmark(
-  parseMunsell(x1, convertColors = FALSE), 
-  parseMunsell(x2, convertColors = FALSE), 
-  parseMunsell(x3, convertColors = FALSE), 
+  '1e3' = parseMunsell(x1, convertColors = FALSE), 
+  '1e4' = parseMunsell(x2, convertColors = FALSE), 
+  '1e5' = parseMunsell(x3, convertColors = FALSE), 
+  '1e6' = parseMunsell(x4, convertColors = FALSE), 
+  '1e7' = parseMunsell(x5, convertColors = FALSE), 
   times = 1
 )
 
