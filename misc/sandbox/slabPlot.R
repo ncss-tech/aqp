726e2f23b06487f6343a9f458904abc11d115726
Dylan Beaudette <debeaudette@ucdavis.edu>
2021-10-14 12:13:47 -0700

    notes

--- misc/sandbox/slabPlot.R
+++ misc/sandbox/slabPlot.R
@@ -1,10 +1,14 @@
 
 
+
+
+
+
 ## TODO:
 # * store key attributes in slab() output
 # * pre-compute most of the manually-specified parameters
 # * lattice AND ggplot2 output
-# * enhanced formular for upper / lower?
+# * enhanced formula for upper / lower?
 
 slabPlot <- function(x, fm, lower = NULL, upper = NULL, ylab = 'Depth (cm)', xlab = 'median bounded by 5th and 95th percentiles', alpha = 0.5, ...) {
   
@@ -26,16 +30,59 @@ slabPlot <- function(x, fm, lower = NULL, upper = NULL, ylab = 'Depth (cm)', xla
     prepanel = prepanel.depth_function,
     ylab = ylab,
     xlab = xlab,
+    par.settings = tactile.theme(superpose.line = list(lwd = 2)),
+    strip = strip.custom(bg = grey(0.85)),
     ...
     
-    
-    
-    
-    
-    # par.settings = tactile.theme(superpose.line = list(lwd = 2)),
-    # strip = strip.custom(bg = grey(0.85))
   )
   
   
    
 }
+
+
+
+
+library(aqp)
+library(lattice)
+library(latticeExtra)
+library(tactile)
+
+
+n <- 20
+
+set.seed(1010101)
+x <- combine(
+  lapply(letters[1:n], random_profile, method = 'LPP', SPC = TRUE)
+)
+
+hzdesgnname(x) <- 'name'
+
+x1 <- trunc(x[1:(n/2), ], z1 = 0, z2 = 30)
+x2 <- x[(n/2)+1:n, ]
+
+
+spc.list <- list(x1, x2)
+args.list <- list(list(color = 'p1'), list(color = 'p1'))
+
+plotMultipleSPC(
+  spc.list, 
+  group.labels = c('fixed depth', 'NRCS'), 
+  max.depth = 105, 
+  args = args.list, 
+  merged.legend = 'p1'
+)
+
+
+site(x1)$group <- 'fixed depth'
+site(x2)$group <- 'NRCS'
+g <- combine(x1, x2)
+
+g$group <- factor(g$group)
+
+
+
+a <- slab(g, fm = group ~ p1 + p2 + p3)
+
+slabPlot(a, fm = top ~ p.q50 | variable, groups = group, lower = a$p.q25, upper = a$p.q75)
+
--- misc/sandbox/soil-color-opinions.R
+++ misc/sandbox/soil-color-opinions.R
@@ -6,19 +6,24 @@ hh <- sample(c('10YR', '7.5YR', '2.5Y'), size = n, replace = TRUE, prob = c(0.7,
 vv <- sample(3:6, size = n, replace = TRUE)
 cc <- sample(3:6, size = n, replace = TRUE)
 
+# opinions of soil color
+m2 <- sprintf('%s %s/%s', hh, vv, cc)
+
+# manually specify opinions
+m2 <- c('10YR 4/4', '7.5YR 4/4', '10YR 3/4', '10YR 4/3', '2.5Y 3/3')
+n <- length(m2)
+
 
 # reference soil color
 m1 <- rep('10YR 4/4', times=n)
 
-# opinions of soil color
-m2 <- sprintf('%s %s/%s', hh, vv, cc)
 
 # compute color contrast metrics for sorting of opinions via dE00
 cc <- colorContrast(m1, m2)
 
-# re-order opinions
-o <- order(cc$dE00)
-m2 <- m2[o]
+# # re-order opinions
+# o <- order(cc$dE00)
+# m2 <- m2[o]
 
 # graphical summary
 par(mar=c(4, 2, 0.25, 0.25), bg='black', fg='white')
