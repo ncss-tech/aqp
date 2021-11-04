f5ffca3f1e6fc2253adb6e875b9d4cb54512fee3
Dylan Beaudette <debeaudette@ucdavis.edu>
2021-11-04 11:55:46 -0700

    adding slab for context

--- misc/sandbox/ols-vs-gls.R
+++ misc/sandbox/ols-vs-gls.R
@@ -7,6 +7,8 @@ library(latticeExtra)
 
 x <- fetchKSSL(c('drummer', 'ava', 'zook'))
 
+x$taxonname <- toupper(x$taxonname)
+
 plotSPC(x, color = 'clay')
 
 x$mintop <- profileApply(x, FUN = function(i) min(i$hzn_top))
@@ -15,7 +17,7 @@ x <- subset(x,  mintop == 0 )
 x <- trunc(x, 0, 100)
 s <- dice(x)
 
-s$taxonname <- toupper(s$taxonname)
+
 
 groupedProfilePlot(s, groups = 'taxonname', color = 'clay', divide.hz = FALSE, name = NA, print.id = FALSE)
 
@@ -27,6 +29,14 @@ h$taxonname <- factor(h$taxonname)
 # # ... how to incorporate error structue?
 # m <- gam(clay ~ s(hzn_top, by = taxonname), data = h)
 # plot(m)
+
+
+# slab
+a <- slab(x, fm = taxonname ~ estimated_ph_h2o)
+
+a <- a[, c('top', 'taxonname', 'p.q50', 'p.q25', 'p.q75')]
+names(a) <- c('hzn_top', 'taxonname', 'linear.predictors', 'lower', 'upper')
+
 
 
 # RMS approach, RCS -> fewer DF used
@@ -47,9 +57,11 @@ p.gls <- predict(m.gls, newdata = nd, conf.int = 0.95)
 p.ols <- cbind(nd, as.data.frame(p.ols))
 p.gls <- cbind(nd, as.data.frame(p.gls))
 
+
 g <- make.groups(
-  OLS = p.ols, 
-  GLS = p.gls
+  'OLS 95% Conf.' = p.ols, 
+  'GLS + corCAR1 95% Conf.' = p.gls,
+  'aqp::slab q25,q50,q75' = a
 )
 
 head(g)
@@ -64,10 +76,11 @@ xyplot(
   ylim = c(105, -5),
   scales = list(alternating = 1),
   panel = panel.depth_function,
-  par.settings = list(superpose.line = list(lwd = 2, col = 2:4)),
+  par.settings = list(superpose.line = list(lwd = 2, col = 2:4), layout.heights = list(strip = 1.45)),
   prepanel = prepanel.depth_function,
   auto.key = list(columns = 3, lines = TRUE, points = FALSE),
-  strip=strip.custom(bg=grey(0.85))
+  strip = strip.custom(bg = grey(0.85)),
+  par.strip.text = list(cex = 0.8),
 )  + latticeExtra::as.layer(xyplot(hzn_top ~ estimated_ph_h2o, groups = taxonname, data = h, cex = 0.125, par.settings = list(superpose.symbol = list(col = 2:4, alpha = 0.33))))
 
 
