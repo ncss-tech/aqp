d4dee74255492d51ab7001a89ee99120c95f891f
Dylan Beaudette <debeaudette@ucdavis.edu>
2021-11-05 11:20:32 -0700

    notes

--- misc/sandbox/ols-vs-gls.R
+++ misc/sandbox/ols-vs-gls.R
@@ -5,21 +5,25 @@ library(rms)
 library(latticeExtra)
 
 
-x <- fetchKSSL(c('drummer', 'ava', 'zook'))
+x <- fetchKSSL(series = c('drummer', 'ava', 'zook'), returnMorphologicData = TRUE, returnGeochemicalData = TRUE, simplifyColors = TRUE)
 
+# checking to make sure this works
+str(x, 1)
+x <- x$SPC
+
+# overly simplistic normalization of taxonname
 x$taxonname <- toupper(x$taxonname)
 
-plotSPC(x, color = 'clay')
-
+# 
 x$mintop <- profileApply(x, FUN = function(i) min(i$hzn_top))
 
 x <- subset(x,  mintop == 0 )
 x <- trunc(x, 0, 100)
 s <- dice(x)
 
-
-
-groupedProfilePlot(s, groups = 'taxonname', color = 'clay', divide.hz = FALSE, name = NA, print.id = FALSE)
+# graphical check
+par(mar = c(0, 0, 3, 0))
+groupedProfilePlot(s, groups = 'taxonname', color = 'clay', divide.hz = FALSE, name = NA, print.id = FALSE, plot.depth.axis = FALSE)
 
 
 h <- as(s, 'data.frame')
@@ -34,12 +38,14 @@ h$taxonname <- factor(h$taxonname)
 # slab
 a <- slab(x, fm = taxonname ~ estimated_ph_h2o)
 
+# re-arrange for stacking with predictions
 a <- a[, c('top', 'taxonname', 'p.q50', 'p.q25', 'p.q75')]
 names(a) <- c('hzn_top', 'taxonname', 'linear.predictors', 'lower', 'upper')
 
 
 
 # RMS approach, RCS -> fewer DF used
+# arcane incantations to make rms functions work
 dd <- datadist(h[, c('hzn_top', 'estimated_ph_h2o', 'taxonname')])
 options(datadist = "dd")
 
@@ -49,15 +55,18 @@ m.ols <- Gls(estimated_ph_h2o ~ rcs(hzn_top, 10) * taxonname, data = h)
 # getting the syntax right is tough
 m.gls <- Gls(estimated_ph_h2o ~ rcs(hzn_top, 10) * taxonname, data = h, correlation = corCAR1(form = ~ hzn_top | pedon_key))
 
+# new data for predictions
 nd <- expand.grid(hzn_top = 1:100, taxonname = levels(h$taxonname))
 
+# predictions + 95% "confidence" interval (confidence in coefficients vs. individual predictions)
 p.ols <- predict(m.ols, newdata = nd, conf.int = 0.95)
 p.gls <- predict(m.gls, newdata = nd, conf.int = 0.95)
 
+# new data + predictions
 p.ols <- cbind(nd, as.data.frame(p.ols))
 p.gls <- cbind(nd, as.data.frame(p.gls))
 
-
+# stack for simpler plotting
 g <- make.groups(
   'OLS 95% Conf.' = p.ols, 
   'GLS + corCAR1 95% Conf.' = p.gls,
