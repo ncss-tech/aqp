59a5193583b7c0b9967ee0f092b1b1bf6da1b813
Dylan Beaudette <debeaudette@ucdavis.edu>
2021-12-01 15:02:25 -0800

    updates

--- misc/sandbox/CIELAB-demonstration.R
+++ misc/sandbox/CIELAB-demonstration.R
@@ -1,8 +1,8 @@
 library(aqp)
 library(lattice)
 
-# remotes::install_github("JosephCrispell/addTextLabels")
-library(addTextLabels)
+# remotes::install_github("JosephCrispell/basicPlotteR")
+library(basicPlotteR)
 
 
 
@@ -114,25 +114,25 @@ x.2 <- subset(munsell, subset=hue == '7.5YR' & value == 3 & chroma  == 4)
 
 
 
-plot(B ~ A, data=x, type='n', las=1)
+plot(B ~ A, data=x, type='n', las=1, asp = 1)
 grid()
 abline(h=0, v=0, lty=3)
 points(B ~ A, data=x, col=rgb(x$r, x$g, x$b, maxColorValue = 1), pch=15, cex=4)
-addTextLabels(x$A, x$B, sprintf("%s %s/%s", x$hue, x$value, x$chroma), cex=0.7, col.background=rgb(0,0,0, 0.1), col.label="black")
+addTextLabels(xCoords = x$A, yCoords = x$B, labels = sprintf("%s %s/%s", x$hue, x$value, x$chroma), cex.label = 0.7, col.background = rgb(0,0,0, 0.1), col.label="black")
 
 
 # par(bg=grey(0.95), mar=c(4.5,4.5,3,1))
 par(bg='white', mar=c(4.5,4.5,3,1))
-plot(B ~ A, data=x, type='n', las=1, main='Munsell Colors in CIELAB\nvalue = 4 | chroma = 6')
+plot(B ~ A, data=x, type='n', las=1, main='Munsell Colors in CIELAB\nvalue = 4 | chroma = 6', asp = 1)
 grid(col='black')
 abline(h=0, v=0, lty=1)
 points(B ~ A, data=x, col=rgb(x$r, x$g, x$b, maxColorValue = 1), pch=15, cex=4)
-addTextLabels(x$A, x$B, x$hue, cex=0.7, col.background=rgb(0,0,0, 0.1), col.label="black")
+addTextLabels(x$A, x$B, x$hue, cex.label = 0.7, col.background=rgb(0,0,0, 0.1), col.label="black")
 
 points(B ~ A, data=x.1, col=rgb(x.1$r, x.1$g, x.1$b, maxColorValue = 1), pch=15, cex=4)
 points(B ~ A, data=x.2, col=rgb(x.2$r, x.2$g, x.2$b, maxColorValue = 1), pch=15, cex=4)
-addTextLabels(x.1$A, x.1$B, sprintf("%s %s/%s", x.1$hue, x.1$value, x.1$chroma), cex=0.7, col.background=rgb(0,0,0, 0.1), col.label="black")
-addTextLabels(x.2$A, x.2$B, sprintf("%s %s/%s", x.2$hue, x.2$value, x.2$chroma), cex=0.7, col.background=rgb(0,0,0, 0.1), col.label="black")
+addTextLabels(x.1$A, x.1$B, sprintf("%s %s/%s", x.1$hue, x.1$value, x.1$chroma), cex.label = 0.7, col.background=rgb(0,0,0, 0.1), col.label="black")
+addTextLabels(x.2$A, x.2$B, sprintf("%s %s/%s", x.2$hue, x.2$value, x.2$chroma), cex.label = 0.7, col.background=rgb(0,0,0, 0.1), col.label="black")
 
 
 par(bg='white', mar=c(4.5,4.5,3,1))
@@ -140,7 +140,7 @@ plot(B ~ A, data=x, type='n', las=1, main='Munsell Colors in CIELAB\nvalue = 4 |
 grid(col='black')
 abline(h=0, v=0, lty=1)
 points(B ~ A, data=x, col=rgb(x$r, x$g, x$b, maxColorValue = 1), pch=15, cex=2)
-addTextLabels(x$A, x$B, x$hue, cex=0.7, col.background=rgb(0,0,0, 0.1), col.label="black")
+addTextLabels(x$A, x$B, x$hue, cex.label = 0.7, col.background=rgb(0,0,0, 0.1), col.label="black")
 
 m.rgb <- t(col2rgb(parseMunsell(soil_minerals$color))) / 255
 m.lab <- convertColor(m.rgb, from='sRGB', to='Lab', from.ref.white = 'D65', clip = FALSE)
@@ -149,20 +149,20 @@ names(m.lab) <- c('L', 'A', 'B')
 m <- data.frame(m.lab, mineral=soil_minerals$mineral, munsell=soil_minerals$color, col=parseMunsell(soil_minerals$color), stringsAsFactors = FALSE)
 
 points(B ~ A, data=m, col=m$col, pch=15, cex=3)
-addTextLabels(m$A, m$B, m$mineral, cex=0.6, col.background=rgb(0,0,0, 0.1), col.label="black")
+addTextLabels(m$A, m$B, m$mineral, cex.label = 0.6, col.background=rgb(0,0,0, 0.1), col.label="black")
 
 
 par(bg='white', mar=c(4.5,4.5,3,1))
-plot(B ~ A, data=m, type='n', las=1, main='Common Soil Pigments', xlab='CIELAB A-Coordinate', ylab='CIELAB B-Coordinate')
+plot(B ~ A, data=m, type='n', las=1, main='Common Soil Pigments', xlab='CIELAB A-Coordinate', ylab='CIELAB B-Coordinate', asp = 1)
 grid(col='black')
 points(B ~ A, data=m, bg=m$col, pch=22, cex=6)
-addTextLabels(m$A, m$B, m$mineral, cex=0.7, col.background=rgb(0,0,0, 0.1), col.label="black")
+addTextLabels(m$A, m$B, m$mineral, cex.label = 0.7, col.background=rgb(0,0,0, 0.1), col.label="black")
 
 par(bg='white', mar=c(4.5,4.5,3,1))
-plot(L ~ A, data=m, type='n', las=1, main='Common Soil Pigments', xlab='CIELAB A-Coordinate', ylab='CIELAB L-Coordinate')
+plot(L ~ A, data=m, type='n', las=1, main='Common Soil Pigments', xlab='CIELAB A-Coordinate', ylab='CIELAB L-Coordinate', asp = 1)
 grid(col='black')
 points(L ~ A, data=m, bg=m$col, pch=22, cex=6)
-addTextLabels(m$A, m$L, m$mineral, cex=0.7, col.background=rgb(0,0,0, 0.1), col.label="black")
+addTextLabels(m$A, m$L, m$mineral, cex.label = 0.7, col.background=rgb(0,0,0, 0.1), col.label="black")
 
 
 
