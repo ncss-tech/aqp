05847c6210d9bc0d24e7e98577f74db931efb261
Dylan Beaudette <debeaudette@ucdavis.edu>
2022-02-24 10:03:29 -0800

    stub for profile_compare() re-write

---
+++ R/NCSP.R
@@ -0,0 +1,13 @@
+
+
+## future home of profile_compare() re-write
+## name suggested by Jon Maynard
+## ideas, commentary, updates c/o Maynard et al. 2020
+
+
+# 
+# NCSP <- function(x) {
+#   
+#   
+# }
+# 
