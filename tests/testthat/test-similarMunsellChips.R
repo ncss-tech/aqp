3b2093eb0c60a19378e125d1e797f2f43e75e6bb
Dylan Beaudette <debeaudette@ucdavis.edu>
2022-03-31 10:35:56 -0700

    updating from latest Munsell LUT, test results are slightly different

--- tests/testthat/test-similarMunsellChips.R
+++ tests/testthat/test-similarMunsellChips.R
@@ -1,16 +1,49 @@
+context("Equivalent Munsell Chips")
+
+
+
 test_that("equivalentMunsellChips works", {
 
   # same "chip page position" different hue page; identify all perceptually equivalent chips
+  
+  # test 1
+  a <- as.list(
+    equivalentMunsellChips("7.5YR", 2, 1)[[1]][, c("hue","value","chroma")]
+    )
+  
+  b <- list(
+    hue = c("10YR", "2.5YR", "5YR", "7.5YR"),
+    value = c(2, 2, 2, 2),
+    chroma = c(1, 1, 1, 1)
+  )
+  
+  expect_equal(a, b)
 
-  expect_equal(as.list(equivalentMunsellChips("7.5YR",2,1)[[1]][,c("hue","value","chroma")]), list(hue = c("10YR", "2.5YR", "5YR", "7.5YR"),
-                                                                                                   value = c(2,2,2,2),
-                                                                                                   chroma = c(1,1,1,1)))
+  
+  # test 2
+  a <- as.list(
+    equivalentMunsellChips("5YR", 2, 1)[[1]][, c("hue","value","chroma")]
+    )
+  
+  b <- list(
+    hue = c("10R", "10YR", "2.5YR", "5YR", "7.5YR"),
+    value = c(2, 2, 2, 2, 2),
+    chroma = c(1, 1, 1, 1, 1)
+  )
+  
+  expect_equal(a, b)
 
-  expect_equal(as.list(equivalentMunsellChips("5YR",2,1)[[1]][,c("hue","value","chroma")]), list(hue = c("10R","2.5YR", "5YR", "7.5YR"),
-                                                                                                 value = c(2,2,2,2),
-                                                                                                 chroma = c(1,1,1,1)))
-
-  expect_equal(as.list(equivalentMunsellChips("10YR",2,1)[[1]][,c("hue","value","chroma")]), list(hue = c("10YR","2.5Y", "7.5YR"),
-                                                                                                  value = c(2,2,2),
-                                                                                                  chroma = c(1,1,1)))
+  # test 3
+  a <- as.list(
+    equivalentMunsellChips("10YR", 2, 1)[[1]][,c("hue", "value", "chroma")]
+  )
+  
+  b <- list(
+    hue = c("10YR", "2.5Y", "5YR", "7.5YR"),
+    value = c(2, 2, 2, 2),
+    chroma = c(1, 1, 1, 1)
+  )
+  
+  expect_equal(a, b)
+  
 })
