429d8f87e2faa36ae615d0f43b9c04525e4017fb
Dylan Beaudette <dylanbeaudette@users.noreply.github.com>
2022-05-25 10:09:51 -0700

    minor update, cleanup
    adding new argument and heuristic to allow for adjusting the horizontal positioning of group delimiting line or arrows

--- R/groupedProfilePlot.R
+++ R/groupedProfilePlot.R
@@ -1,28 +1,44 @@
-#' Grouped Soil Profile Plot
+#' @title Grouped Soil Profile Plot
 #'
-#' Plot a collection of soil profiles, sorted and labeled by group.
+#' @description Plot a collection of soil profiles, sorted by group.
 #'
 #' The left-right ordering of groups can be adjusted by converting
 #' \code{groups} into a factor and explicitly setting factor levels.
 #' Alpha-numeric ordering is used for all other types.
 #'
-#' @param x a \code{SoilProfileCollection} object
+#' @param x a `SoilProfileCollection` object
+#' 
 #' @param groups the name of a site-level attribute that defines groups, factor
 #' levels will influence plotting order
+#' 
 #' @param group.name.offset vertical offset for group names, single numeric
 #' value or vector of offsets
+#' 
 #' @param group.name.cex font size for group names
+#' 
 #' @param group.line.col color for line that splits groups
+#' 
 #' @param group.line.lwd width of line that splits groups
+#' 
 #' @param group.line.lty style of line that splits groups
+#' 
 #' @param break.style style of group boundaries: "line", "arrow", "both"
+#' 
+#' @param break.offset horizontal offset used to place vertical breaks and/or arrows, shifted slightly to the right of default when `hz.depths=TRUE` is passed to `plotSPC()`
+#' 
 #' @param arrow.offset vertical offset for "arrow" style boundaries, single
 #' numeric value or vector of offsets
-#' @param arrow.length value passed to \code{arrows} to define arrow head size
-#' @param \dots further arguments to \code{plotSPC}
+#' 
+#' @param arrow.length value passed to `arrows` to define arrow head size
+#' 
+#' @param \dots further arguments to `plotSPC`
+#' 
 #' @author D.E. Beaudette
+#' 
 #' @seealso \code{\link{plotSPC}}
+#' 
 #' @keywords hplots
+#' 
 #' @examples
 #'
 #' # sample data
@@ -99,16 +115,29 @@
 #' group.line.lwd = 1)
 #' }
 #'
-groupedProfilePlot <- function(x, groups, group.name.offset=-5, group.name.cex=0.75, group.line.col='RoyalBlue', group.line.lwd=2, group.line.lty=2, break.style='line', arrow.offset=group.name.offset + 5, arrow.length=0.1, ...) {
+groupedProfilePlot <- function(x, groups, group.name.offset = -5, group.name.cex = 0.75, group.line.col = 'RoyalBlue', group.line.lwd = 2, group.line.lty = 2, break.style = c('line', 'arrow', 'both'), break.offset = 0.5, arrow.offset = group.name.offset + 5, arrow.length = 0.1, ...) {
 
   # sanity check, `groups` must be a site-level attribute
   if(! any(groups %in% siteNames(x))) {
     stop(sprintf("%s is not a site-level attribute", groups), call. = FALSE)
   }
 
+  # sanity check
+  break.style <- match.arg(break.style)
+  
   # extract site data, used for ordering and labeling
   s <- site(x)
 
+  # break.offset needs to be shifted to the right a little bit, when hz.depths = TRUE is specified
+  if('hz.depths' %in% names(list(...)) && list(...)[['hz.depths']]) {
+    # only make a change if break.offset isn't specified
+    if(missing(break.offset)){
+      # shift to the right slightly
+      # message('`hz.depths=TRUE`, shifting `break.offset` to the right')
+      break.offset <- 0.62
+    }
+  }
+  
   # if groups are already a factor, keep existing levels
   # note that an ordered factor has multiple classes: "ordered" , "factor"
   if(! any(inherits(s[[groups]], 'factor'))) {
@@ -116,7 +145,7 @@ groupedProfilePlot <- function(x, groups, group.name.offset=-5, group.name.cex=0
   }
 
   # derive ordering based on `groups`
-  # alpha sorting when `gopups` is numeric or character
+  # alpha sorting when `grpups` is numeric or character
   # factor levels when `groups` is a factor
   new.order <- order(s[[groups]])
   lab <- s[[groups]][new.order]
@@ -145,10 +174,10 @@ groupedProfilePlot <- function(x, groups, group.name.offset=-5, group.name.cex=0
   group.lengths <- rle(as.numeric(lab))$lengths
 
   # label positions
-  lab.positions <- (cumsum(group.lengths) - (group.lengths / 2)) + 0.5
+  lab.positions <- (cumsum(group.lengths) - (group.lengths / 2)) + break.offset
 
   # group boundaries on x-axis
-  boundary.positions <-  cumsum(group.lengths)[-length(group.lengths)] + 0.5
+  boundary.positions <-  cumsum(group.lengths)[-length(group.lengths)] + break.offset
 
   # reasonable upper / lower boundaries on y-axis
   # these are informed by plotting parameters sent to plotSPC()
@@ -157,8 +186,9 @@ groupedProfilePlot <- function(x, groups, group.name.offset=-5, group.name.cex=0
   
   upper.position <- (.yoffset) + (group.name.offset/2 * lsp$scaling.factor)
   lower.position <- (.yoffset) + (lsp$max.depth * lsp$scaling.factor)
-
-  if(length(boundary.positions)) { # only add grouping symbols if number of groups is > 1
+  
+  # only add grouping symbols if number of groups is > 1
+  if(length(boundary.positions)) {
     # add group boundaries
     if(break.style == 'line')
       segments(y0 = upper.position, y1=lower.position, x0=boundary.positions, x1=boundary.positions, lty=group.line.lty, lwd=group.line.lwd, col=group.line.col)
@@ -173,6 +203,6 @@ groupedProfilePlot <- function(x, groups, group.name.offset=-5, group.name.cex=0
   }
 
   # annotate with group labels
-  text(lab.positions, group.name.offset, unique.lab, cex=group.name.cex, adj=c(0.75, 0), font=4)
+  text(lab.positions, group.name.offset, unique.lab, cex = group.name.cex, adj = c(0.75, 0), font = 4)
 }
 
