6b2a8b21bba60374efe623665d5a96c33c725439
Andrew G. Brown <andrew.g.brown@usda.gov>
2022-06-22 16:48:14 -0700

    generalize.hz ideas  - S4 methods, new alias  - ordered factors

--- R/generalize.hz.R
+++ R/generalize.hz.R
@@ -8,24 +8,21 @@
 #' Generalize Horizon Names
 #'
 #' Generalize a vector of horizon names, based on new classes, and REGEX
-#' patterns.
+#' patterns. Or create a new column `gendesgnname` in a SoilProfileCollection (requires a horizon designation name to be defined for the collection, see details)
 #'
-#'
-#' @param x a character vector of horizon names
+#' @param x a character vector of horizon names or a `SoilProfileCollection`
 #' @param new a character vector of new horizon classes
-#' @param pat a character vector of REGEX, same length as \code{x}
-#' @param non.matching.code text used to describe any horizon not matching any
-#' item in \code{pat}
-#' @param hzdepm a numeric vector of horizon mid-points, must not contain NA,
-#' same length as \code{x}
-#' @param \dots additional arguments passed to \code{grep()} such as
-#' \code{perl=TRUE} for advanced REGEX
-#' @return factor of the same length as \code{x}
+#' @param pattern a character vector of REGEX patterns, same length as `new`
+#' @param non.matching.code label used for any horizon not matching any `pattern`
+#' @param hzdepm a numeric vector of horizon mid-points; `NA` values in `hzdepm` will result in `non.matching.code` (or `NA` if not defined) in result
+#' @param ordered by default, the result is an ordered factor when `hzdepm` is defined. 
+#' @param ... additional arguments passed to `grep()` such as `perl=TRUE` for advanced REGEX
+#' @return (ordered) factor of the same length as `x` (if character) or as number of horizons in `x` (if SoilProfileCollection)
+#' @details When `x` is a SoilProfileCollection the `gendesgnname` column will be updated with the factor results. This requires that the "horizon designation name" metadata be defined for the collection to set the column for input designations. See `hzdesgnname()`.
+#' @seealso `hzdesgnname()`
 #' @author D.E. Beaudette
 #' @keywords manip
 #' @examples
-#'
-#' \dontrun{
 #' 
 #' data(sp1)
 #' 
@@ -35,7 +32,7 @@
 #' # generalize
 #' sp1$genhz <- generalize.hz(sp1$name,
 #'                            new=c('O','A','B','C','R'),
-#'                            pat=c('O', '^A','^B','C','R'))
+#'                            pattern=c('O', '^A','^B','C','R'))
 #' 
 #' # see how we did / what we missed
 #' table(sp1$genhz, sp1$name)
@@ -54,39 +51,82 @@
 #' p <- c('A', '^A', '(?<!A)C')
 #' 
 #' # note additional argument
-#' res <- generalize.hz(x, new = n, pat=p, perl=TRUE)
+#' res <- generalize.hz(x, new = n, pattern=p, perl=TRUE)
 #' 
 #' # double-check: OK
 #' table(res, x)
 #' 
-#' }
 #'
-generalize.hz <- function(x, new, pat, non.matching.code='not-used', hzdepm = NA, ...) {
+generalize.hz <- function(x, new, pattern, non.matching.code = 'not-used', hzdepm = NULL, ordered = !missing(hzdepm), ...) {
 
 	# init vector of 'other', same length as original horizon name vector
-	g <- rep(non.matching.code, times=length(x))
+	g <- rep(non.matching.code, times = length(x))
 
 	# iterate over new new names, match with patterns, assign new hz names
-	for(i in seq_along(new)) {
-		g[grep(pat[i], x, ...)] <- new[i]
+	for (i in seq_along(new)) {
+		g[grep(pattern[i], x, ...)] <- new[i]
 	}
 
-	# convert to factor with levels set by median depth
-	# TODO: this will fail if a given genhz label is missing hz mid-points
-	if(! all(is.na(hzdepm))) {
+	# convert to (ordered) factor with levels set by median depth
+	if (!missing(hzdepm) && !is.null(hzdepm) && !all(is.na(hzdepm))) {
+	  # more stringent NA handling:
+	  # stopifnot(`Number of non-NA \`hzdepm\` does match number of new levels` =
+	  #           { sum(!is.na(hzdepm)) == length(new) })
+	  
+	  # less stringent:
+	  # any NA hzdepm will return NA factor level, even when pattern is matched
 	  new_sort <- names(sort(tapply(hzdepm, g, median)))
 	  new_sort <- new_sort[new_sort != non.matching.code]
-	  g <- factor(g, levels = c(new_sort, non.matching.code))
-
-	  return(g)
+	  
+	  # use an ordered factor (may be overridden w/ ordered = FALSE)
+	  g <- factor(g, levels = c(new_sort, non.matching.code), ordered = ordered)
+	  
+	  # if any are not matched (i.e. hzdepm is NA), replace with non.matching.code (if defined)
+	  if (!is.null(non.matching.code)) {
+	    g[is.na(g)] <- non.matching.code
+	  }
+	  g
+	} else {
+  	# no adjustment of factor levels based on hz mid-points
+  	# no order implied (by default; override with ordered=TRUE)
+  	factor(g, levels = c(new, non.matching.code), ordered = ordered)
 	}
-
-	# no adjustment of factor levels based on hz mid-points
-	g <- factor(g, levels = c(new, non.matching.code))
-	return(g)
-
 }
 
+setGeneric("generalizeHz",  function(x, new, pattern, non.matching.code, hzdepm, ordered, ...) standardGeneric("generalizeHz"))
+
+#' @export
+#' @rdname generalize.hz
+setMethod("generalizeHz", signature(x = "character"), function(x, new, pattern, non.matching.code = 'not-used', hzdepm = NULL, ordered = !missing(hzdepm), ...) {
+ generalize.hz(
+    x,
+    new = new,
+    pattern = pattern,
+    non.matching.code = 'not-used',
+    hzdepm = hzdepm,
+    ordered = ordered, 
+    ...
+  )
+})
+
+# SoilProfileColletion method
+#' @param gendesgnname Generalized Horizon Designation column name (to be created/updated when `x` is a `SoilProfileCollection`)
+#' @export
+#' @rdname generalize.hz
+setMethod("generalizeHz", signature(x = "SoilProfileCollection"), function(x, new, pattern, non.matching.code = 'not-used', hzdepm = NULL, ordered = !missing(hzdepm), gendesgnname = "genhz", ...) {
+  hzdesgn <- hzdesgnname(x, required = TRUE)
+  
+  x[[gendesgnname]] <- generalize.hz(
+    x[[hzdesgn]],
+    new = new,
+    pattern = pattern,
+    non.matching.code = 'not-used',
+    hzdepm = hzdepm,
+    ordered = ordered, 
+    ...
+  )
+  x
+})
 
 # convert a cross-tabulation: {original, genhz} to adjacency matrix
 genhzTableToAdjMat <- function(tab) {
