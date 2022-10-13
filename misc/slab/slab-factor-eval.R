9643d32c9fbeceb7cc8574960d5a3118cfad19a8
Andrew G. Brown <andrew.g.brown@usda.gov>
2022-10-13 11:51:51 -0700

    slab: add cpm=2 comparison

--- misc/slab/slab-factor-eval.R
+++ misc/slab/slab-factor-eval.R
@@ -6,18 +6,6 @@ library(aqp)
 # all VALID
 x <- readRDS('misc/slab/clarksville-pedons-final.rds')
 
-# keep track of generalized horizon names for later
-hz.names <- levels(x$genhz)
-
-# slice out color and horizon name into 1cm intervals: no aggregation
-max.depth <- 180
-slice.resolution <- 1
-slice.vect <- seq(from = 0, to = max.depth, by = slice.resolution)
-s <- slice(x, slice.vect ~ genhz)
-
-# convert horizon name to factor
-s$genhz <- factor(s$genhz, levels = hz.names)
-
 # compute slice-wise probability: slice-wise P always sum to 1
 a <- slab(x, ~ genhz, cpm = 1)
  
@@ -26,17 +14,20 @@ a <- slab(x, ~ genhz, cpm = 1)
 
 a.1 <- slab(x, ~ genhz, cpm = 1)
 a.2 <- slab(x, ~ genhz, cpm = 2)
+
 # saveRDS(a.1, file = 'misc/slab/slab-factor-1x-cpm1.rds')
 # saveRDS(a.2, file = 'misc/slab/slab-factor-1x-cpm2.rds')
-saveRDS(a.1, file = 'misc/slab/slab-factor-2x-cpm1.rds')
-saveRDS(a.2, file = 'misc/slab/slab-factor-2x-cpm2.rds')
-
-# saveRDS(a, file = 'misc/slab/slab-factor-1x-cpm2.rds')
-# saveRDS(a, file = 'misc/slab/slab-factor-2x.rds')
+# saveRDS(a.1, file = 'misc/slab/slab-factor-2x-cpm1.rds')
+# saveRDS(a.2, file = 'misc/slab/slab-factor-2x-cpm2.rds')
 
 library(daff)
 
 d1 <- readRDS('misc/slab/slab-factor-1x.rds')
 d2 <- readRDS('misc/slab/slab-factor-2x.rds')
+diff_data(d1,d2) |> 
+  render_diff()
+
+d1 <- readRDS('misc/slab/slab-factor-1x-cpm2.rds')
+d2 <- readRDS('misc/slab/slab-factor-2x-cpm2.rds')
 diff_data(d1,d2) |> 
   render_diff()
