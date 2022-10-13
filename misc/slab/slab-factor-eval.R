686b73a7aeade04525c03ed92d993c04a8ed3e4a
Andrew G. Brown <andrew.g.brown@usda.gov>
2022-10-13 11:46:29 -0700

    slab: update cpm=2 comparison

--- misc/slab/slab-factor-eval.R
+++ misc/slab/slab-factor-eval.R
@@ -4,14 +4,30 @@ library(aqp)
 
 ## pre-cached/subset data
 # all VALID
-x <- readRDS('clarksville-pedons-final.rds')
-
+x <- readRDS('misc/slab/clarksville-pedons-final.rds')
 
 # compute slice-wise probability: slice-wise P always sum to 1
-a.1 <- slab(x, ~ genhz, cpm = 1)
-
-a.2 <- slab(x, ~ genhz, cpm = 2)
+a <- slab(x, ~ genhz, cpm = 1)
  
-# saveRDS(a, file = 'slab-factor-1x-cpm2.rds')
+# saveRDS(a, file = 'misc/slab/slab-factor-1x.rds')
+# saveRDS(a, file = 'misc/slab/slab-factor-2x.rds')
 
-# saveRDS(a, file = 'e:/temp/slab-factor-2x.rds')
+a.1 <- slab(x, ~ genhz, cpm = 1)
+a.2 <- slab(x, ~ genhz, cpm = 2)
+
+# saveRDS(a.1, file = 'misc/slab/slab-factor-1x-cpm1.rds')
+# saveRDS(a.2, file = 'misc/slab/slab-factor-1x-cpm2.rds')
+# saveRDS(a.1, file = 'misc/slab/slab-factor-2x-cpm1.rds')
+# saveRDS(a.2, file = 'misc/slab/slab-factor-2x-cpm2.rds')
+
+library(daff)
+
+d1 <- readRDS('misc/slab/slab-factor-1x.rds')
+d2 <- readRDS('misc/slab/slab-factor-2x.rds')
+diff_data(d1,d2) |> 
+  render_diff()
+
+d1 <- readRDS('misc/slab/slab-factor-1x-cpm2.rds')
+d2 <- readRDS('misc/slab/slab-factor-2x-cpm2.rds')
+diff_data(d1,d2) |> 
+  render_diff()
