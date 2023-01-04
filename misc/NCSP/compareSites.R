3b0c94b08697902e77ba5b7dfe3f67d741f0e8ce
Beaudette <dylan.beaudette@ca.usda.gov>
2023-01-04 09:13:52 -0800

    ideas

---
+++ misc/NCSP/compareSites.R
@@ -0,0 +1,34 @@
+
+devtools::load_all()
+library(sharpshootR)
+
+
+data("sierraTransect")
+
+# TODO: rebuild this, contains old-style @sp and other issues
+x <- sierraTransect
+x <- rebuildSPC(x)
+
+hzdesgnname(x) <- 'name'
+
+par(mar = c(0, 0, 0, 1))
+plotSPC(x, color = 'Fe_o_to_Fe_d', name.style = 'center-center')
+
+groupedProfilePlot(x, groups = 'transect', color = 'Fe_o_to_Fe_d', name.style = 'center-center', group.name.offset = -15)
+
+
+d1 <- NCSP(x, vars = 'Fe_o_to_Fe_d', rescaleResult = TRUE)
+
+par(mar = c(0, 0, 0, 1))
+plotProfileDendrogram(x, clust = cluster::diana(d1), color = 'Fe_o_to_Fe_d', width = 0.3, scaling.factor = 0.008, name.style = 'center-center')
+
+d2 <- compareSites(x, vars = c('MAP', 'MAAT'))
+
+par(mar = c(0, 0, 0, 1))
+plotProfileDendrogram(x, clust = cluster::diana(d2), color = 'Fe_o_to_Fe_d', width = 0.3, name.style = 'center-center')
+
+
+d <- (d1 + d2) / 2
+
+par(mar = c(0, 0, 0, 1))
+plotProfileDendrogram(x, clust = cluster::diana(d), color = 'Fe_o_to_Fe_d', width = 0.3, name.style = 'center-center')
