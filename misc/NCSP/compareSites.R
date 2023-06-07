f12fc4902a451fcc3f061f7a594b8816b5458a18
Beaudette <dylan.beaudette@usda.gov>
2023-06-07 13:20:17 -0700

    cleanup

--- misc/NCSP/compareSites.R
+++ misc/NCSP/compareSites.R
@@ -5,33 +5,40 @@ library(sharpshootR)
 
 data("sierraTransect")
 
-# TODO: rebuild this, contains old-style @sp and other issues
+# local copy
 x <- sierraTransect
-x <- rebuildSPC(x)
 
-hzdesgnname(x) <- 'name'
+.a <- list(color = 'Fe_o_to_Fe_d', name.style = 'center-center', plot.depth.axis = FALSE, hz.depths = TRUE, width = 0.3, cex.names = 0.75, cex.id = 0.66)
+options(.aqp.plotSPC.args = .a)
 
-par(mar = c(0, 0, 0, 1))
-plotSPC(x, color = 'Fe_o_to_Fe_d', name.style = 'center-center')
+par(mar = c(0, 0, 0, 0))
+plotSPC(x)
 
-groupedProfilePlot(x, groups = 'transect', color = 'Fe_o_to_Fe_d', name.style = 'center-center', group.name.offset = -15)
+groupedProfilePlot(x, groups = 'transect' , group.name.offset = -15)
+
+
+plot(site(x)[, c('MAP_800', 'MAAT_800')])
+
+plotSPC(x, plot.order = order(x$effective.ppt_800))
 
 
 d1 <- NCSP(x, vars = 'Fe_o_to_Fe_d', rescaleResult = TRUE)
 
-par(mar = c(0, 0, 0, 1))
-plotProfileDendrogram(x, clust = cluster::diana(d1), color = 'Fe_o_to_Fe_d', width = 0.3, scaling.factor = 0.008, name.style = 'center-center')
+# result is within [0,1]
+d2 <- compareSites(x, vars = c('MAP_800', 'MAAT_800'))
 
-d2 <- compareSites(x, vars = c('MAP', 'MAAT'))
 
-par(mar = c(0, 0, 0, 1))
-plotProfileDendrogram(x, clust = cluster::diana(d2), color = 'Fe_o_to_Fe_d', width = 0.3, name.style = 'center-center')
-
-w1 <- 2
+w1 <- 1
 w2 <- 1
 
 ## idea: following symbolic representation used in SEM ... I think
 d <- (w1*d1 + w2*d2) / sum(c(w1, w2))
 
-par(mar = c(0, 0, 0, 1))
-plotProfileDendrogram(x, clust = cluster::diana(d), color = 'Fe_o_to_Fe_d', width = 0.3, name.style = 'center-center')
+
+plotProfileDendrogram(x, clust = cluster::diana(d1), max.depth = 180, width = 0.3, scaling.factor = 0.008)
+
+plotProfileDendrogram(x, clust = cluster::diana(d2), max.depth = 180, width = 0.3, scaling.factor = 0.008)
+
+
+
+plotProfileDendrogram(x, clust = cluster::diana(d), max.depth = 180, width = 0.3, scaling.factor = 0.006)
