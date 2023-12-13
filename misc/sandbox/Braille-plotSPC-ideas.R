0e98686c59d7a053aca1b805e533958b1c8f3554
Beaudette <dylan.beaudette@usda.gov>
2023-12-13 14:22:51 -0800

    Update Braille-plotSPC-ideas.R

--- misc/sandbox/Braille-plotSPC-ideas.R
+++ misc/sandbox/Braille-plotSPC-ideas.R
@@ -28,7 +28,8 @@ font_add('BRAILLE1', regular = 'S:/NRCS/Archive_Dylan_Beaudette/AQP-related/SPC-
 
 showtext_auto()
 
-svglite(file = 'E:/working_copies/ncss-tech.github.io/AQP/SVG-examples/braille-example.svg', width = 16, height = 12, pointsize = 14, standalone = TRUE)
+
+svglite(file = 'E:/working_copies/ncss-tech.github.io/AQP/SVG-examples/braille-example.svg', width = 17, height = 12, pointsize = 14)
 
 # pdf(file = 'braille-example.pdf', width = 16, height = 12, pointsize = 14)
 
@@ -38,6 +39,7 @@ par(mar = c(0, 0, 0, 0), family = 'BRAILLE1')
 # SoilTaxonomyDendrogram(h, y.offset = 0.325, name = 'newname', width = 0.3, cex.taxon.labels = 0.55, name.style = 'center-center', hz.depths =  TRUE, plot.depth.axis = FALSE, cex.id = 0.66, cex.names = 0.55)
 
 # plot profiles
-plotSPC(h, width = 0.3, name.style = 'center-center', hz.depths =  TRUE, depth.axis = FALSE, cex.id = 0.66, cex.names = 0.55, color = NA)
+plotSPC(h, width = 0.33, name.style = 'center-center', hz.depths = TRUE, hz.depths.offset = 0.1, depth.axis = FALSE, cex.id = 0.66, cex.names = 0.66, color = NA, max.depth = 155, lwd = 1.5)
 
 dev.off()
+
