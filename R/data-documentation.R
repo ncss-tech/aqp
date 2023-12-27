df4244c2f5e86d905a67d559b03ab207f80d0d54
Beaudette <dylan.beaudette@usda.gov>
2023-12-27 13:34:06 -0800

    doc cleanup

--- R/data-documentation.R
+++ R/data-documentation.R
@@ -3,6 +3,25 @@
 ## don't forget: @usage data(XXX)
 
 
+#' @title Lookup tables for sand, silt, clay, texture class, and textural modifiers.
+#' 
+#' @description A list that contains a snapshot of the values generated using the logic from
+#' the particle size estimator calculation in NASIS, the average values per
+#' texture class, and average rock fragment values by textural modifier.
+#' 
+#' @usage data(soiltexture)
+#' @format A list with 3 data frames. The first named values which contains
+#' values for sand, silt and clay by texture class. The second with average
+#' values for sand, silt and clay per texture class. The third has fragvoltot
+#' low, rv and high values for texmod.  \describe{ \item{list("clay")}{clay
+#' percentage of the fine earth fraction, a integer vector}
+#' \item{list("sand")}{sand percentage of the fine earth fraction, a integer
+#' vector} \item{list("silt")}{silt percentage of the fine earth fraction, a
+#' integer vector} \item{list("texcl")}{texture class, a character vector}
+#' \item{list("texmod")}{textural modifiers, a character vector} }
+#' @keywords datasets
+"soiltexture"
+
 #' @title Example Output from soilDB::fetchOSD()
 #' @description An example `SoilProfileCollection` object created by `soilDB::fetchOSD()`, derived from the Cecil, Appling, and Bonneau Official Series Descriptions.
 #' @keywords datasets
