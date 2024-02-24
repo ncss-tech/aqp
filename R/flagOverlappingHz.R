336144672048c538cad339491e59e672d2108b9e
Andrew Gene Brown <brown.andrewg@gmail.com>
2024-02-24 13:14:22 -0800

    flagOverlappingHz: properly check bottom depths

--- R/flagOverlappingHz.R
+++ R/flagOverlappingHz.R
@@ -5,9 +5,13 @@
 #'
 #' @return logical vector with length (and order) matching the horizons of `x` 
 #' 
-#' @author D.E. Beaudette
+#' @author D.E. Beaudette, A.G. Brown
 #' 
 #' @export
+#' @details
+#' Horizons with `NA` depths are are not flagged as overlapping. Find and remove or fix these with `checkHzDepthLogic(byhz=TRUE)` if needed.
+#' 
+#' @seealso [checkHzDepthLogic()] [fillHzGaps()]
 #'
 #' @examples
 #' 
@@ -32,29 +36,28 @@ flagOverlappingHz <- function(x) {
   h <- horizons(x)
   hzd <- horizonDepths(x)
   
-  # TODO: horizons with NA depths are not flagged as overlapping
-  # TODO: bottom depths are never used or compared
+  # NOTE: horizons with NA depths are not flagged as overlapping
+  #  - rle gives length 1 for each NA; could recode NA values before rle?
   
   .tops <- h[[hzd[1]]]
-  # .bottoms <- h[[hzd[2]]]
+  .bottoms <- h[[hzd[2]]]
+  .rt <- rle(.tops) 
+  .rb <- rle(.bottoms)
   
-  .rt <- rle(.tops) # NOTE: rle gives length 1 for each NA
-  # .rb <- rle(.bottoms)
-  
+  # TODO: need alternative to rle() lengths here
   .ot <- .rt$values[which(.rt$lengths > 1)]
-  # .ob <- .rb$values[which(.rt$lengths > 1)]
+  .ob <- .rb$values[which(.rb$lengths > 1)]
   
   # index affected horizons
-  # TODO: handle NA in logical comparisons
+  # TODO: handle NA in logical comparisons?
   .m1 <- outer(.ot, .tops, '==') 
-  # .m2 <- outer(.ob, .bottoms, '==') 
+  .m2 <- outer(.ob, .bottoms, '==')
   idx1 <- unlist(as.vector(apply(.m1, 1, which)))
-  # idx2 <- unlist(as.vector(apply(.m2, 1, which)))
+  idx2 <- unlist(as.vector(apply(.m2, 1, which)))
   
   # generate flag vector along sequence of horizons 
   .res <- rep(FALSE, times = length(.tops))
-  # .res[intersect(idx1, idx2)] <- TRUE
-  .res[idx1] <- TRUE
+  .res[intersect(idx1, idx2)] <- TRUE
   .res
 }
 
