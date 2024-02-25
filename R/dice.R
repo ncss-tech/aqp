ce0b6332170b9a7f538f09497e04496857cf5f67
Andrew Gene Brown <brown.andrewg@gmail.com>
2024-02-25 15:16:46 -0800

    use base R for formula extraction

--- R/dice.R
+++ R/dice.R
@@ -65,9 +65,9 @@ setGeneric("dice", function(x,
     }
     
     # extract components of the formula:
-    fm <- stringr::str_c(deparse(fm, 500), collapse = "")
-    elements <- stringr::str_split(fm, stringr::fixed("~"))[[1]]
-    fm <- lapply(stringr::str_split(elements, "[^e]{1}[+*]"), str_trim)
+    fm <- paste0(deparse(fm, 500), collapse = "")
+    elements <- strsplit(fm, "~", fixed = TRUE)[[1]]
+    fm <- lapply(strsplit(elements, "[^e]{1}[+*]"), trimws)
     
     # test for a multi-part formula A ~ B ~ C ?
     if (length(fm) > 2) {
