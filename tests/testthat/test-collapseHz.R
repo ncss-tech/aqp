5bcac0734a6b941065f0c34e630b8e29c0c96bb4
Andrew Gene Brown <andrew.g.brown@usda.gov>
2024-10-11 13:54:19 -0700

    add weighted average and dominant condition tests, with and without NA

--- tests/testthat/test-collapseHz.R
+++ tests/testthat/test-collapseHz.R
@@ -9,6 +9,9 @@ test_that("collapseHz works", {
   # calculate a new SPC with genhz column based on patterns
   jacobs2000_gen <- generalizeHz(jacobs2000, new = new_labels, pattern = patterns)
 
+  # create a missing value
+  jacobs2000_gen$clay[19] <- NA
+  
   # collapse that SPC based on genhz
   i <- collapseHz(jacobs2000_gen, hzdesgn = "genhz")
   expect_equal(length(jacobs2000), length(i))
@@ -17,15 +20,44 @@ test_that("collapseHz works", {
 
   # collapses adjacent horizons with same label
   i <- collapseHz(jacobs2000_gen, by = "genhz")
+  ii <- collapseHz(jacobs2000_gen, by = "genhz", na.rm = TRUE)
   
   # no effect, horizon designations are unique within profiles
   j <- collapseHz(jacobs2000_gen, by = "name")
   
-  expect_equal(length(jacobs2000), length(i))
-  expect_equal(nrow(i), 26)
   expect_equal(nrow(j), 46)
-  expect_equal(i[7, , .BOTTOM], c(15, 41, 61, 132, 140, 152))
   expect_equal(j[7, , .BOTTOM], jacobs2000[7, , .BOTTOM])
+  
+  # if using `by` argument, all values must not be NA
+  expect_error(collapseHz(jacobs2000_gen, by = "matrix_color_munsell"),
+               "Missing values are not allowed")
+  
+  # matches input number of profiles
+  expect_equal(length(jacobs2000), length(i))
+  
+  # horizons have been collapsed
+  expect_equal(nrow(i), 26)
+  
+  # weighted mean (no NA values) works as expected (clay=47.15)
+  expect_equal(i$clay[4],
+               weighted.mean(jacobs2000_gen$clay[6:7], (jacobs2000_gen$bottom - jacobs2000_gen$top)[6:7]))
+  
+  # weighted mean (contains NA values, na.rm=FALSE) (clay is NA)
+  expect_true(is.na(i$clay[11]))
+  
+  # weighted mean (contains NA values, na.rm=TRUE, clay=18.72414)
+  expect_equal(ii$clay[11],
+               weighted.mean(jacobs2000_gen$clay[17:20], (jacobs2000_gen$bottom - jacobs2000_gen$top)[17:20], na.rm = TRUE))
+  
+  # dominant condition (NA values retained)
+  expect_true(is.na(i$depletion_munsell[13]))
+  
+  # dominant condition (NA values removed)
+  expect_equal(ii$depletion_munsell[13], "10YR 8/2")
+  
+  plot(jacobs2000_gen, color = "concentration_pct")
+  
+  expect_equal(i[7, , .BOTTOM], c(15, 41, 61, 132, 140, 152))
   expect_true(is.numeric(i$clay))
   expect_true(is.numeric(j$clay))
   
