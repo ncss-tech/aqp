f7322bfe4b0959c7e8875b3b12a303baacb1385b
Stephen Roecker <stephen.roecker@usda.gov>
2025-09-26 13:05:25 -0500

    adding mu_confusion_matrix()

--- R/accuracy_uncertainty.R
+++ R/accuracy_uncertainty.R
@@ -165,3 +165,70 @@ brierScore <- function(x, classLabels, actual = 'actual') {
   return(bs)
 }
 
+
+#' Map unit confusion matrix and other classification measures
+#' 
+#' This function reverse engineers a confusion matrix and other classification measures from soil map unit component percentages (i.e. composition) and area (i.e. acres).
+#' @param x `data.frame`
+#' @param mapunit `character` column name containing the mapunit identifier (e.g. nationalmusym)
+#' @param cophase `character` column name containing the soil component phase identifier (e.g. coiid or paste(compname, localphase))
+#' @param comppct `character` column name containint the component percent (e.g. comppct_r)
+#' @param muacres `character` column name containing the total area of the mapunit (e.g. muacres)
+#' 
+#' @details `mu_confusion_matrix` There are several common statistical measures used to gauge the accuracy of categorical maps. These measures are typically not estimated for soil surveys but can be inferred from a map unit’s soil component composition percentages and size (i.e. acres). In general, overall purity or accuracy (OP) is related to map unit kind (e.g. consociations vs complexes). For several reasons, the “true” accuracies are unknown, and these values should be interpreted as Bayesian prior estimates. However, it is likely that the estimates are optimistic if they haven't been derived from an external validation. Existing and future digital soil mapping products could be used to validate how optimistic the current OA estimates are.
+#'
+#' @return `list` a confusion matrix, overall purity (OP) (i.e. overall accuracy), map unit purity (MP) (i.e. user accuracy), class representation (CR) (i.e. producer accuracy), and Shannon entropy (E). The measure names were selected to be consistent with the alternative terms proposed by Lark (1995) and Brus (2011). 
+#' 
+#' @author Stephen Roecker
+#' 
+#' #' @references
+#'
+#'    - Brus DJ, Kempen B, Heuvelink GBM. 2011. Sampling for validation of digital soil maps. European Journal of Soil Science. 62(3):394–407. \doi{10.1111/j.1365-2389.2011.01364.x}
+#'    - Lark RM. 1995. Components of accuracy of maps with special reference to discriminant analysis on remote sensor data. International Journal of Remote Sensing. 16(8):1461–1480. \doi{10.1080/01431169508954488}
+#' 
+#' @seealso [tauW()], [shannonEntropy()], [confusionIndex()]
+#' 
+#' @export
+#'
+#' @examples
+#' 
+#' # example data
+#' mu <- rbind(
+#'     data.frame(mapunit = "A", cophase = c("Alpha", "Beta"), comppct = c(90, 10), muacres = 100),
+#'     data.frame(mapunit = "B", cophase = c("Beta", "Alpha"), comppct = c(70, 30), muacres = 1000)
+#'     )
+#' 
+#' mu_confusion_matrix(mu, mapunit = "mapunit", cophase = "cophase", comppct = "comppct")
+#' 
+
+mu_confusion_matrix <- function(x, mapunit = "nationalmusym", cophase = "coiid", comppct = "comppct_r", muacres = "muacres") {
+  
+  vars <- c(mapunit = mapunit, cophase = cophase, comppct = comppct, muacres = muacres)
+  idx  <- sapply(vars, function(y) which(names(x) %in% y))
+  names(x)[idx] <- names(vars)
+  x <- x[names(vars)]
+  
+  x$coacres <- x$muacres * x$comppct/100
+  
+  CM <- stats::xtabs(coacres ~ mapunit + cophase, data = x)
+  rowmax <- apply(CM, 1, max)
+  colmax <- apply(CM, 2, max)
+  MP <- rowmax / rowSums(CM)
+  CR <- colmax / colSums(CM)
+  OP <- sum(rowmax) / sum(CM)
+  H  <- apply(CM, 1, function(x) aqp::shannonEntropy(x/sum(x)))
+  CM <- CM |> cbind(MP, H) |>  rbind(CR = c(CR, c(NA, NA)))
+  names(dimnames(CM)) <- c("map unit", "component phase")
+  
+  return(
+    list(
+      confusion_matrix     = CM |> round(2), 
+      overall_purity       = OP, 
+      map_purity           = MP, 
+      class_representation = CR, 
+      shannon_entropy      = H)
+  )
+}
+
+
+
